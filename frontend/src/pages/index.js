dab7f789c0f09af0a9402d0c1c930e726e6a3dfe
Maple Xu <maplexu2010@gmail.com>
2023-06-16 15:01:04 -0400

    bind src files for webpack compatibility

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1 className="text-3xl font-bold underline">
-        Welcome to <b>Gatsby!</b>
+        Welcome to <b>Gatsby2!</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
