4e46fc1412172c2172cc2c11c92dc185d75d5527
Josh Cheng <jcheng@duosecurity.com>
2023-06-30 09:28:54 -0400

    Use inline tailwind

--- frontend/src/components/page-about/index/leadership.js
+++ frontend/src/components/page-about/index/leadership.js
@@ -1,51 +1,56 @@
 import * as React from "react"
-import {
-  titleContainer,
-  container,
-  elderListContainer,
-  elderCard,
-  elderImg,
-  elderName,
-  buttonContainer,
-} from "./leadership.module.css"
-import ImgPeteDahlem from "../../../images/about-elders-pete-dahlem.png";
-import ImgJoshYang from "../../../images/about-elders-josh-yang.png";
-import ImgDaveYon from "../../../images/about-elders-dave-yon.png";
-import ImgSeongPark from "../../../images/about-elders-seong-park.png";
+import imgPeteDahlem from "../../../images/about-elders-pete-dahlem.png";
+import imgJoshYang from "../../../images/about-elders-josh-yang.png";
+import imgDaveYon from "../../../images/about-elders-dave-yon.png";
+import imgSeongPark from "../../../images/about-elders-seong-park.png";
 import {ButtonLink} from "../../Button";
 
-const LeadershipSection = () => (
-  <div className={container}>
-    <div className={titleContainer}>
+
+
+const LeadershipSection = () => {
+  const elderInfo = [
+    {
+      img: imgPeteDahlem,
+      name: "Rev. Pete Dahlem",
+      role: "Lead Pastor",
+    },
+    {
+      img: imgJoshYang,
+      name: "Josh Yang",
+      role: "Associate Pastor",
+    },
+    {
+      img: imgDaveYon,
+      name: "Dave Yon",
+      role: "Elder",
+    },
+    {
+      img: imgSeongPark,
+      name: "Seong Park",
+      role: "Elder",
+    }
+  ];
+
+  const elderCards = elderInfo.map((info) => (
+    <div className="flex flex-col items-start">
+      <img className="mb-3" src={info.img} alt={info.name} />
+      <h3 className="font-bold text-2xl leading-tighter mb-3">{info.name}</h3>
+      <div>{info.role}</div>
+    </div>
+  ));
+
+  return <div className="pb-20.25 flex flex-col items-center">
+    <div className="text-center pb-11.25">
       <div className="subheading">Our Leadership</div>
       <h2>Meet Our Elders</h2>
     </div>
-    <div className={elderListContainer}>
-      <div className={elderCard}>
-        <img className={elderImg} src={ImgPeteDahlem} alt="Rev. Pete Dahlem" />
-        <h3 className={elderName}>Rev. Pete Dahlem</h3>
-        <div>Lead Pastor</div>
-      </div>
-      <div className={elderCard}>
-        <img className={elderImg} src={ImgJoshYang} alt="Josh Yang" />
-        <h3 className={elderName}>Josh Yang</h3>
-        <div>Associate Pastor</div>
-      </div>
-      <div className={elderCard}>
-        <img className={elderImg} src={ImgDaveYon} alt="Dave Yon" />
-        <h3 className={elderName}>Dave Yon</h3>
-        <div>Elder</div>
-      </div>
-      <div className={elderCard}>
-        <img className={elderImg} src={ImgSeongPark} alt="Seong Park" />
-        <h3 className={elderName}>Seong Park</h3>
-        <div>Elder</div>
-      </div>
+    <div className="flex justify-center gap-4 pb-11.25">
+      {elderCards}
     </div>
-    <div className={buttonContainer}>
+    <div className="flex">
       <ButtonLink to={"/about/our-team"} hasArrow={true}>More About Our Elders and Deacons</ButtonLink>
     </div>
   </div>
-);
+};
 
 export default LeadershipSection
