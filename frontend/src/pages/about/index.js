116853c6b6fdc4554525487b3ab281cdcb718559
jchenggggg <joshcheng36@gmail.com>
2023-06-30 08:54:28 -0400

    Move about.js to about/index.js and add scaffolding for main pages (#12)
    * Move about.js to about/index.js and add scaffolding for main pages

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -2,14 +2,14 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import Partners from "../components/partners"
-import Strategy from "../components/strategy"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import Partners from "../../components/partners"
+import Strategy from "../../components/strategy"
 
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="About" />;
 
-export default () => (
+const AboutPage = () => (
   <Layout>
     <div>Our Story section</div>
     <div>Our Mission section</div>
@@ -19,4 +19,6 @@ export default () => (
     <Partners/>
     <div>Feedback Form section</div>
   </Layout>
-)
+);
+
+export default AboutPage
