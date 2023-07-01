61b3c90a9a2c49d08d70c4fc8864ed973d9ec798
HMCC Ann Arbor <135241924+hmccaa@users.noreply.github.com>
2023-07-01 01:05:54 -0400

    [16] Add infrastructure for documentation (#14)
    * docs init
    
    * test action
    
    * test action
    
    * test
    
    ---------
    
    Co-authored-by: Maple Xu <maplexu2010@gmail.com>

---
+++ docs/_book/serve.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker stop docs
+docker run -it --init -dp 4000:4000  -v `pwd`:`pwd` -w `pwd` --name docs --rm $(docker build -t docs -q .) honkit serve
+# sed -e "s/<li>\n        <a href=\"https:\/\/github.com\/honkit\/honkit\" target=\"blank\" class=\"gitbook-link\">\n            Published with HonKit\n        <\/a>\n    <\/li>//g" -i.backup ./_book/index.html
+
+sleep 5
+sed -e "s/github.com\/honkit\/honkit/annarbor.hmcc.net/g" -i.backup $SCRIPT_DIR/_book/*.html
+sed -e "s/Published with HonKit/HMCCAA Website Docs/g" -i.backup $SCRIPT_DIR/_book/*.html
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# HMCCAA Website Documentation
+
+This is the main documentation hub for everything regarding the HMCC Ann Arbor Website.
+
+## Contributing
+
+To contribute,
