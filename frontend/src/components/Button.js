395b9e93995e2fb9080571b5144610ee147fac36
thedomcab <9808478+thedomcab@users.noreply.github.com>
2023-07-05 19:11:20 -0400

    Update HTML + CSS to use SVG + CSS (#16)
    * changes to use svgs+css over html+css and images
    
    * updates for missing svg path

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,36 +1,18 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import {
-  button,
-  textContainer,
-  arrowContainer,
-  top,
-  bottom,
-  line,
-} from "../css/button.module.css"
+import * as React from "react";
+import { Link } from "gatsby";
+import { ButtonArrow } from "./svgs";
+import { button, textContainer } from "../css/button.module.css";
 
 export const ButtonLink = ({ to = "#", hasArrow = false, children }) => (
   <Link className={button} to={to}>
     <span className={textContainer}>{children}</span>{" "}
-    {hasArrow && (
-      <span className={arrowContainer}>
-        <div className={top}></div>
-        <span className={line}>&nbsp;</span>
-        <div className={bottom}></div>
-      </span>
-    )}
+    {hasArrow && <ButtonArrow />}
   </Link>
-)
+);
 
 export const Button = ({ hasArrow = false, type, children }) => (
   <button type={type} className={button}>
     <span className={textContainer}>{children}</span>{" "}
-    {hasArrow && (
-      <span className={arrowContainer}>
-        <div className={top}></div>
-        <span className={line}>&nbsp;</span>
-        <div className={bottom}></div>
-      </span>
-    )}
+    {hasArrow && <ButtonArrow />}
   </button>
-)
+);
