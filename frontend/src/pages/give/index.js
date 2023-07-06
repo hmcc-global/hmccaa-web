34556cb11f691c483651d13b37e013520484ad6b
Josh Cheng <jcheng@duosecurity.com>
2023-07-06 17:14:55 -0400

    Rebase

--- frontend/src/pages/give/index.js
+++ frontend/src/pages/give/index.js
@@ -4,13 +4,15 @@ import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import WhyWeGiveSection from "../../components/page-give/why-we-give-section";
 
-export const Head = () => <Seo title="Give" />;
-
 const GivePage = () => (
   <Layout>
-    <WhyWeGiveSection/>
-    <div>Ways to give section</div>
+      <div className="max-w-container">
+        <WhyWeGiveSection/>
+        <div>Ways to give section</div>
+      </div>
   </Layout>
 );
+
+export const Head = () => <Seo title="Give" />;
 
 export default GivePage
