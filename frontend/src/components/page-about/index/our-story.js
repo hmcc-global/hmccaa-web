50d55ad708722ca4c50987989c774359856f29b6
Brandt Bowling <brandt.bowling@gmail.com>
2023-07-09 21:28:11 -0400

    reformat some styling

---
+++ frontend/src/components/page-about/index/our-story.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+const OurStory = () => {
+  return (
+    <div className="flex flex-col items-start gap-14">
+      <div>
+        <div className="flex flex-col items-start gap-3">
+          <div className="subheading">Our Story</div>
+          <h1 className="text-4xl font-semibold text-Neutral-900 uppercase">
+            Transform lives, Transform the world.
+          </h1>
+        </div>
+        <div>
+          <p>
+            Harvest Mission Community Church began in 1996 with a group of eight
+            people and a dream: to be a 1st-century church in the 21st century.
+            As we looked into the book of Acts, God gave us the vision to be a
+            community of faith that raises bold disciples of Christ who will
+            transform the world. We planted our roots on the University of
+            Michigan campus in Ann Arbor with a focus of reaching college
+            students, because we believe that college is a pivotal time for
+            people to discover their God-given destiny. Over the years, our
+            ministry has expanded to encompass people from different walks of
+            life—from infants and youth, to single adults and married couples,
+            to international students and families. Although the composition of
+            our church may continue to change, our mission will always remain
+            the same. We desire to see people’s lives transformed, redirected to
+            Gods purposes, and set out to transform the world for the glory of
+            God.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OurStory
--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -7,13 +7,14 @@ import Strategy from "../../components/page-about/index/strategy"
 import Belief from "../../components/page-about/index/belief"
 import LeadershipSection from "../../components/page-about/index/leadership";
 import Feedback from "../../components/page-about/index/feedback"
+import OurStory from "../../components/page-about/index/our-story"
 
 export const Head = () => <Seo title="About" />;
 
 const AboutPage = () => (
   <Layout>
     <div className="max-w-container">
-      <div>Our Story section</div>
+      <OurStory/>
       <div>Our Mission section</div>
       <div>Our Values section</div>
       <LeadershipSection/>
