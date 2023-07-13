d0fe6345909a61f49ce14ce08323ef5148bfdbb7
Maple Xu <maplexu2010@gmail.com>
2023-07-12 22:47:14 -0400

    sermon schema on strapi

---
+++ cms/src/api/preacher/controllers/preacher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * preacher controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::preacher.preacher');
