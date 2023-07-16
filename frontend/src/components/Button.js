31a023dcc22caa31c008134ac3c4e4a9058bb0d1
graceschae <gchae@umich.edu>
2023-07-15 21:54:06 -0400

    resolve conflict button.js and pages/index.js

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,34 +1,36 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import { ButtonArrow } from "./svgs";
-import { button, textContainer, secondaryButton, primaryButton } from "../css/button.module.css";
+import * as React from "react"
+import { Link } from "gatsby"
+import { ButtonArrow } from "./svgs"
+import {
+  button,
+  textContainer,
+  secondaryButton,
+  primaryButton,
+} from "../css/button.module.css"
 
-export const ButtonLink = ({ to = "#", hasArrow = false, className = "", children }) => (
+export const ButtonLink = ({
+  to = "#",
+  hasArrow = false,
+  className = "",
+  children,
+}) => (
   <Link className={`${button} ${className}`} to={to}>
     <span className={textContainer}>{children}</span>{" "}
     {hasArrow && <ButtonArrow />}
   </Link>
-);
+)
 
 export const Button = ({ hasArrow = false, type, children }) => (
   <button type={type} className={button}>
     <span className={textContainer}>{children}</span>{" "}
     {hasArrow && <ButtonArrow />}
   </button>
-);
+)
 
-export const SecondaryButtonLink = (props) => (
-  <ButtonLink
-    {...props}
-    className={`${props.className} ${secondaryButton}`}
-  />
-);
+export const SecondaryButtonLink = props => (
+  <ButtonLink {...props} className={`${props.className} ${secondaryButton}`} />
+)
 
-export const PrimaryButtonLink = (props) => (
-  <ButtonLink
-    {...props}
-    className={`${props.className} ${primaryButton}`}
-  />
-);
-
-
+export const PrimaryButtonLink = props => (
+  <ButtonLink {...props} className={`${props.className} ${primaryButton}`} />
+)
