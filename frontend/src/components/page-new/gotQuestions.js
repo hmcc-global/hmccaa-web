c185eff243b5adcb7c4b7e952e07317c9e508491
graceschae <gchae@umich.edu>
2023-07-20 18:13:55 -0400

    fix header syntax

--- frontend/src/components/page-new/gotQuestions.js
+++ frontend/src/components/page-new/gotQuestions.js
@@ -5,14 +5,14 @@ const Questions = () => (
   <div className="pt-14 pb-14">
     <div className="flex gap-x-10 pt-5 pb-5 pl-28 pr-28 place-items-center border-2 border-Neutral-600  rounded-xl">
       <div>
-        <h className="text-5xl font-semibold">GOT ANY QUESTIONS?</h>
+        <h2 className="text-5xl font-semibold">GOT ANY QUESTIONS?</h2>
       </div>
       <div className="flex items-center">
-        <h>Shoot us an email at: </h>
+        <h3>Shoot us an email at: </h3>
         <span className="inline-flex items-center mr-1 ml-2">
           <MailIcon className="w-6 h-6" />
         </span>{" "}
-        <h className="font-bold">annarbor@hmcc.net</h>
+        <h3 className="font-bold">annarbor@hmcc.net</h3>
       </div>
     </div>
   </div>
