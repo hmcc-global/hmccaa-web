f542d5761d88d4a61ca80691bde8d7e55481ad2a
graceschae <gchae@umich.edu>
2023-07-19 22:49:58 -0400

    pretty.js for last commit

--- frontend/src/pages/next-steps/index.js
+++ frontend/src/pages/next-steps/index.js
@@ -6,8 +6,8 @@ import LifeGroup from "../../components/page-next-steps/lifeGroup";
 
 const NextStepsPage = () => (
   <Layout>
-    <div className="max-w-container mx-auto">n
-      <div>Life Groups section</div>
+    <div className="max-w-container mx-auto">
+      n<div>Life Groups section</div>
       <div>Membership section</div>
       <div>Transformation classes section</div>
       <div>Baptism section</div>
