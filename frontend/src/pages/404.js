a6973f408a548fe32a8f244f2070febd6dca66e0
yijo <yijo@umich.edu>
2023-07-21 12:25:02 -0400

    Made Small Changes to 404 Error page and added 404 module css

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -2,11 +2,24 @@ import * as React from "react";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { SecondaryButtonLink } from "../components/Button";
+import {
+  container
+} from "../css/404.module.css";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={container}>
+      <div className="text-center pb-11.25">
+        <h1>404: Not Found</h1>
+        <div className="subheading">You just hit a route that doesn&#39;t exist... the sadness.</div>
+      </div>
+      <div className="flex">
+        <SecondaryButtonLink to={"/"} hasArrow={true}>
+          Home
+        </SecondaryButtonLink>
+      </div>
+    </div>s
   </Layout>
 );
 
