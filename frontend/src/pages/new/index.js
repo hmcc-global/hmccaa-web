ac2bc74f5bc4a7faee17889eec94c7607a1a7512
graceschae <gchae@umich.edu>
2023-07-29 10:48:46 -0400

    got rid of lifestage in index for now

--- frontend/src/pages/new/index.js
+++ frontend/src/pages/new/index.js
@@ -3,13 +3,11 @@ import * as React from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 import Questions from "../../components/page-new/gotQuestions";
-import LifeStages from "../../components/page-new/lifeStages";
 
 const NewcomersPage = () => (
   <Layout>
     <div>Welcome section</div>
     <div>Sunday celebration bar</div>
-    <LifeStages />
     <Questions />
   </Layout>
 );
