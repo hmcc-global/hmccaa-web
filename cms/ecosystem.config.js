5693cf27ba01e9790e18d06d0ea4ce9c5833916c
Maple Xu <maplexu2010@gmail.com>
2023-07-30 19:01:02 -0400

    comments

--- cms/ecosystem.config.js
+++ cms/ecosystem.config.js
@@ -1,3 +1,13 @@
+// This file is used for configuring the `pm2` process which manages our
+// CMS binary's lifetime on the server.
+// <https://pm2.keymetrics.io/docs/usage/application-declaration/>
+
+// To start:
+// `pm2 start ecosystem.config.js`
+//
+// To restart:
+// `pm2 restart strapi`
+
 module.exports = {
   apps: [
     {
