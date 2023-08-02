ce46d9ab075066216a7c77356d1de2f677de9cbe
Lucy Huang <lucyhuan@umich.edu>
2023-08-02 15:33:28 -0400

    Finished desktop view and partially done with mobile for header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,102 +1,106 @@
 import * as React from "react";
 import hmccLogo from "../images/hmcc-ripple-white.svg";
 import mapPinLogo from "../images/icons/map-pin.svg";
+import dropDown from "../images/icons/dropdown.svg";
 
 const Header = () => {
-    const browseList = [
-      { title: "About", route: "/about" },
-      { title: "Connect", route: "/connect" },
-      { title: "Next Steps", route: "/next-steps" },
-      { title: "Events", route: "/events" },
-      { title: "Watch", route: "/watch" },
-    ];
+  const browseList = [
+    { title: "About", route: "/about" },
+    { title: "Connect", route: "/connect" },
+    { title: "Next Steps", route: "/next-steps" },
+    { title: "Events", route: "/events" },
+    { title: "Watch", route: "/watch" },
+    { title: "Give", route: "/give" },
+  ];
 
-    const containerStyle = {
-      display: "flex",
-      height: "100px",
-      padding: "40px 80.286px 17px 80px",
-      justifyContent: "center",
-      gap: "246px",
-      alignItems: "flex-start",
-      background: "var(--primary-1700, #0C2966)",
-    };
-    
-    const textStyle = {
-      color: "#FFF",
-      fontFamily: "Raleway",
-      fontSize: "16px",
-      fontStyle: "normal",
-      lineHeight: "normal",
-      textDecoration: "none",
-    };
+  const locationsList = [
+    { title: "Ann Arbor", route: "/" },
+    { title: "Austin", route: "https://austin.hmcc.net/" },
+    { title: "Detroit", route: "https://detroit.hmcc.net/" },
+    { title: "Hong Kong", route: "https://hongkong.hmcc.net/" },
+    { title: "Jakarta", route: "https://jakarta.hmcc.net/" },
+  ];
 
-    const borderStyle = {
-      display: "flex",
-      padding: "12px 20px",
-      alignItems: "flex-start",
-      gap: "8px",
-      borderRadius: "8px",
-      border: "2px solid var(--accent-1500, #FF8069)",
-      boxShadow: "0px 4px 12px 0px rgba(0, 0, 0, 0.16)",
-    };
-    
-    return (
-      <header>
-        <div style={containerStyle}>
-          <div className="flex flex-row items-center gap-[12px]">
-            <a href="\">
-              <img
-                alt="hmcc logo"
-                className="w-[80px] h-[32px] mb-0"
-                src={hmccLogo}
-              />
-            </a>
-          
-            <a style={textStyle}
-              className="text-left text-sm leading-normal w-[183px] font-semibold">
-              HARVEST MISSION COMMUNITY CHURCH
-            </a>
-          </div>
-          <div className="flex flex-row items-center gap-[40px]">
-              <div>
-                <a
-                  href="https://www.google.com/maps/place/Harvest+Mission+Community+Church/@42.2816338,-83.7372209,17z/data=!4m5!3m4!1s0x883cae6a77eef201:0xaf4019d9fc7aec8e!8m2!3d42.2816359!4d-83.7371982?hl=en&shorturl=1"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="hover:opacity-75"
-                >
-                  <img
-                    alt="map pin logo"
-                    className="mb-0"
-                    src={mapPinLogo}
-                  />
-                </a>
-              </div>
-            <div className="items-start flex flex-row gap-[40px]">
-              {browseList.map((item, index) => (
+  const [isOpen, setIsOpen] = React.useState(false);
+  const path = window.location.pathname;
+  const containerStyle = "flex flex-row h-[60px] [@media(min-width:900px)]:h-[100px] flext-start items-center justify-center bg-Primary-700 pl-[20px] xl:pl-[80.286px] [@media(min-width:900px)]:pt-[40px] [@media(min-width:900px)]:pb-[17px] pr-[20px] xl:pr-[65px] lg:gap-[20px] xl:gap-[140px] [@media(min-width:1440px)]:gap-[220px]"
+  const logoStyle = "w-[80px] h-[32px] mb-0";
+  const dropDownStyle = "w-[40px] h-[40px] mb-0"
+  const textStyle = "text-Shades-0 text-base leading-normal no-underline";
+  const mapPinStyle = "mb-0 shrink-0 h-[20px] w-[15.7px] peer hover:bg-[#1A56D6]";
+  const borderStyle = "flex items-center py-2 px-4 md:px-6 rounded-lg border-2 border-[#FF8069] shadow-md";
+
+  return (
+    <header>
+      <div className={containerStyle}>
+        <div className="flex flex-row w-full justify-between [@media(min-width:900px)]:hidden">
+          <a href="/" className="content-start">
+            <img alt="hmcc logo" className={logoStyle} src={hmccLogo} />
+          </a>
+          <a href="/" className={`${textStyle} text-center w-[183px] font-semibold`}>
+            HARVEST MISSION COMMUNITY CHURCH
+          </a>
+          <a href="/">
+            <img alt="drop down" className={dropDownStyle} src={dropDown} />
+          </a>
+        </div>
+
+        <div className="hidden [@media(min-width:900px)]:flex items-center gap-4 md:gap-6 lg:gap-8">
+          <a href="/">
+            <img alt="hmcc logo" className={logoStyle} src={hmccLogo} />
+          </a>
+          <a href="/" className={`${textStyle} w-[183px] font-semibold`}>
+            HARVEST MISSION COMMUNITY CHURCH
+          </a>
+        </div>
+
+        <div className="flex-row items-center gap-4 hidden [@media(min-width:900px)]:flex [@media(min-width:1040px)]:gap-8 [@media(min-width:1440px)]:gap-11">
+          <div className="relative hover:bg-[#1A56D6] hover:border-[#1A56D6] hover:rounded-t-lg">
+            <div className="hover:bg-[#1A56D6] hover:border-[#1A56D6] py-2 px-2 rounded-t-lg peer">
+              <img alt="map pin logo" className={`${mapPinStyle}`} src={mapPinLogo}/>
+            </div>
+            <div className="hidden peer-hover:flex hover:flex w-[150px] flex-col absolute bg-[#1A56D6]">
+              {locationsList.map((item, index) => (
                 <a
                   key={`browseLink-${index}`}
                   href={item.route}
-                  style={textStyle}
-                  className="font-medium hover:opacity-75 tracking-[.96px]"
+                  target="_blank"
+                  className={`${textStyle} hover:bg-[#0C2966] py-2 px-4 border-b-[0.1px] border-gray-100 tracking-[0.96px]`}
                 >
                   {item.title}
                 </a>
               ))}
-            </div>
-            <div style={borderStyle}>
-              <a href="/new"
-                 style={textStyle}
-                 className="font-bold hover:opacity-75 tracking-[.96px]">
-                    I'm New
+              <a 
+                  href = "https://tangerang.hmcc.net/"
+                  target="_blank"
+                  className={`${textStyle} hover:bg-[#0C2966] py-2 px-4 tracking-[0.96px]`}
+                >
+                  Tangerang
               </a>
             </div>
           </div>
-          
+
+          <div className="flex flex-row items-center gap-4 md:gap:6 [@media(min-width:1040px)]:gap-8 [@media(min-width:1440px)]:gap-11">
+            {browseList.map((item, index) => (
+              <a
+                key={`browseLink-${index}`}
+                href={item.route}
+                className={`${textStyle} ${item.route + "/" == path ? "font-extrabold hover:font-extrabold" : "font-normal hover:font-bold"} tracking-[0.96px]`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+
+          <div className={borderStyle}>
+            <a href="/new" className={`${textStyle} ${"/new/" == path ? "font-extrabold" : ""} font-bold hover:font-extrabold tracking-[0.96px]`}>
+              I'm New
+            </a>
+          </div>
         </div>
-        
-      </header>
-    );
-  };
-  
-  export default Header;
+      </div>
+    </header>
+  );
+};
+
+export default Header;
