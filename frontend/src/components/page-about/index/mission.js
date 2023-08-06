dc1dc14823d177f687a744d01df22df010e75a04
terrianx <txiaopk@gmail.com>
2023-08-06 00:14:51 -0700

    Completed missions

--- frontend/src/components/page-about/index/mission.js
+++ frontend/src/components/page-about/index/mission.js
@@ -12,29 +12,31 @@ import {
 
 const Mission = () => (
   <div className={container}>
-  <div className={topBox}>
-    <div className={topLeftTriangle}></div>
-    <div className={text}>
-      <div className="subheading">What we are called to do</div>
-      <h2>Our mission</h2>
-      <p className="text-Secondary-900">
-        To transform lost people into Christ’s 
-        disciples who will then transform the world.
-      </p>
+    <div className={topBox}>
+      <div className={topLeftTriangle}></div>
+      <div className={text}>
+        <div className="subheading">What we are called to do</div>
+        <h2>Our mission</h2>
+        <p>
+          To <span>transform lost people</span> into Christ’s 
+          disciples who will then <span>transform the world</span>.
+        </p>
+      </div>
+      <div className={topRightTriangle}></div>
     </div>
-    <div className={topRightTriangle}></div>
+    <div className={bottomBox}>
+      <div className={bottomLeftTriangle}></div>
+      <div className={text}>
+        <div className="subheading">How we aspire to live</div>
+        <h2>Our vision</h2>
+        <p>
+          Becoming a <span>transcultural spiritual 
+          family</span> reaching our neighbors, the 
+          nations, and the next generation.
+        </p>
+      </div>
+      <div className={bottomRightTriangle}></div>
+    </div>
   </div>
-  <div className="bottom-box">
-    <div className="bottom-left-triangle"></div>
-    <div className="subheading">How we aspire to live</div>
-    <h2>Our vision</h2>
-    <p>
-      Becoming a transcultural spiritual family 
-      reaching our neighbors, the nations, and 
-      the next generation.
-    </p>
-    <div className="bottom-right-triangle"></div>
-  </div>
-</div>
 );
 export default Mission;
