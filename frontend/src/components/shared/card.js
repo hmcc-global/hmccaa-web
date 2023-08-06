a2c97576ad37816b42b03bbd384cd0d4672d48c5
bbowling <brandt.bowling@gmail.com>
2023-08-05 20:40:33 -0400

    responsive design first bit

--- frontend/src/components/shared/card.js
+++ frontend/src/components/shared/card.js
@@ -4,24 +4,24 @@ import mapPinLogo from "../../images/icons/map-pin.svg";
 
 const Card = ({ type, date, name, location, description }) => {
   return (
-    <div className="flex flex-col items-center gap-2 max-w-[9.75rem] max-h-[12.5rem] pb-[2.19rem] lg:max-w-[23.75rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden">
+    <div className="flex flex-col items-center gap-2 min-w-[9.75rem] min-h-[12.5rem] pb-[2.19rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden md:min-w-[23.75rem] md:min-h-[30rem] md:pb-[3.13rem] md:gap-5">
       <img
-        className="h-[6.15788rem] w-[10.94738rem] flex-shrink-0 mb-0 md:h-full md:w-48"
+        className="h-[6.15788rem] w-[10.94738rem] flex-shrink-0 mb-0 md:h-60 md:w-[26.66669rem]"
         src={prayerGatheringImg}
         alt="Prayer Gathering"
       />
-      <div className="flex flex-col gap-1 items-start max-w-[8.75rem] max-h-[3.63rem]">
-        <div className="text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem]">
+      <div className="flex flex-col gap-1 items-start min-w-[8.75rem] min-h-[3.63rem] md:gap-3 md:min-w-[20rem]">
+        <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           Th, May 25, 2023
-        </div>
-        <div className="text-lg leading-[1.25rem] font-semibold text-black">
+        </p>
+        <h3 className="text-lg leading-[1.25rem] font-semibold text-black md:leading-[1.8rem] md:text-2xl">
           Prayer Gathering
-        </div>
+        </h3>
         <div className="flex items-center gap-1">
           <img className="w-4 h-4 mb-0" src={mapPinLogo} alt="Map" />
-          <div className="text-black text-[0.625rem] leading-[0.9375rem] font-medium tracking-[0.0375rem]">
+          <p className="mb-0 text-black text-[0.625rem] leading-[0.9375rem] font-medium tracking-[0.0375rem] md:text-base md:leading[1.3125rem] md:tracking-[0.0525rem]">
             T-Center
-          </div>
+          </p>
         </div>
       </div>
     </div>
