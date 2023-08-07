6ae877e6189d6e5f9ac2927fb70749123a83f273
terrianx <txiaopk@gmail.com>
2023-08-06 23:53:21 -0700

    Redesigned missions component to use a single trapezoid polygon

--- frontend/src/components/page-about/index/mission.js
+++ frontend/src/components/page-about/index/mission.js
@@ -3,39 +3,37 @@ import {
   container,
   text,
   topBox,
-  topLeftTriangle,
-  topRightTriangle,
+  topTrapezoid,
   bottomBox,
-  bottomLeftTriangle,
-  bottomRightTriangle,
+  bottomTrapezoid
 } from "../../../css/mission.module.css";
 
 const Mission = () => (
   <div className={container}>
     <div className={topBox}>
-      <div className={topLeftTriangle}></div>
-      <div className={text}>
-        <div className="subheading">What we are called to do</div>
-        <h2>Our mission</h2>
-        <p>
-          To <span>transform lost people</span> into Christ’s 
-          disciples who will then <span>transform the world</span>.
-        </p>
+      <div className={topTrapezoid}>
+        <div className={text}>
+          <div className="subheading">What we are called to do</div>
+          <h2>Our mission</h2>
+          <p>
+            To <span>transform lost people</span> into Christ’s 
+            disciples who will then <span>transform the world</span>.
+          </p>
+        </div>
       </div>
-      <div className={topRightTriangle}></div>
     </div>
     <div className={bottomBox}>
-      <div className={bottomLeftTriangle}></div>
-      <div className={text}>
-        <div className="subheading">How we aspire to live</div>
-        <h2>Our vision</h2>
-        <p>
-          Becoming a <span>transcultural spiritual 
-          family</span> reaching our neighbors, the 
-          nations, and the next generation.
-        </p>
+      <div className={bottomTrapezoid}>
+        <div className={text}>
+          <div className="subheading">How we aspire to live</div>
+          <h2>Our vision</h2>
+          <p>
+            Becoming a <span>transcultural spiritual 
+            family</span> reaching our neighbors, the 
+            nations, and the next generation.
+          </p>
+        </div>
       </div>
-      <div className={bottomRightTriangle}></div>
     </div>
   </div>
 );
