7c7a44a9b94bb621e4a5769a82afcc4e85e937c6
graceschae <gchae@umich.edu>
2023-08-07 12:10:21 -0400

    resolve conflicts 3

--- frontend/src/components/lifeStages.js
+++ frontend/src/components/lifeStages.js
@@ -20,17 +20,19 @@ const LifeStages = () => (
       <h3 className="subheading">life stages</h3>
       <h2>there&apos;s a place for everyone</h2>
     </div>
-    
+
     <div className={mainContainer}>
       {/* <div className="my-20"> */}
       {/* className="object-scale-down" */}
       <div className="my-20">
         <div className={subContainer}>
-          <img className="object-scale-down w-44" src={LifeStageImg1} /> 
+          <img className="object-scale-down w-44" src={LifeStageImg1} />
           <div className="textbox text-right">
-              <h3 className={textTitle}>A PLACE FOR SINGLE ADULTS</h3>
-              <h4 className={textBody}>New to the area? Just graduated? Our single 
-              adult group is for anyone out of school and not yet married. </h4>
+            <h3 className={textTitle}>A PLACE FOR SINGLE ADULTS</h3>
+            <h4 className={textBody}>
+              New to the area? Just graduated? Our single adult group is for
+              anyone out of school and not yet married.{" "}
+            </h4>
           </div>
         </div>
 
@@ -38,9 +40,11 @@ const LifeStages = () => (
           {/* <img className="object-scale-down w-44" src={LifeStageImg2} /> */}
           <div className="textbox text-right">
             <h3 className={textTitle}>A PLACE FOR UNDERGRADS</h3>
-            <h4 className={textBody}>A student at U of M? Eastern? Other schools for 
-              undergrad studies? Our undergrad ministry is what sparked 
-              the start of HMCC. We would love to connect more with you!</h4>
+            <h4 className={textBody}>
+              A student at U of M? Eastern? Other schools for undergrad studies?
+              Our undergrad ministry is what sparked the start of HMCC. We would
+              love to connect more with you!
+            </h4>
           </div>
         </div>
       </div>
@@ -48,33 +52,34 @@ const LifeStages = () => (
         <div className={subContainer}>
           <div className="textBox text-left">
             <h3 className={textTitle}>A PLACE FOR FAMILIES</h3>
-            <h4 className={textBody}>Newly married? Have newborns? Kids? 
-              We have families from different life stages 
-              you can connect with!</h4>
+            <h4 className={textBody}>
+              Newly married? Have newborns? Kids? We have families from
+              different life stages you can connect with!
+            </h4>
           </div>
           <img className="object-scale-down w-44" src={LifeStageImg3} />
-
         </div>
         <div className={subContainer}>
           <div className="textBox text-left">
             <h3 className={textTitle}>A PLACE FOR GRAD STUDENTS</h3>
-            <h4 className={textBody}>In grad school? Taking extra courses? 
-              This is a place you can connect with those 
-              pursuing higher education.</h4>
+            <h4 className={textBody}>
+              In grad school? Taking extra courses? This is a place you can
+              connect with those pursuing higher education.
+            </h4>
           </div>
           <img className="object-scale-down w-44" src={LifeStageImg4} />
         </div>
         <div className={subContainer}>
           <div className="textBox text-left">
             <h3 className={textTitle}>A PLACE FOR INTERNATIONAL STUDENTS</h3>
-            <h4 className={textBody}>Flew in from overseas to school in the US? 
-              We have students from China, HK, Indonesia, Malaysia, Japan, 
-              and many more!</h4>
+            <h4 className={textBody}>
+              Flew in from overseas to school in the US? We have students from
+              China, HK, Indonesia, Malaysia, Japan, and many more!
+            </h4>
           </div>
           <img className="object-scale-down w-44" src={LifeStageImg5} />
         </div>
       </div>
-      
     </div>
     <p className="font-bold text-base text-center">
       please email abc@hmccaa.com to get connected!
