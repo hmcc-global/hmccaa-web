36042b75c7c0667013d3ba5fc29fa109e5b4afb1
bbowling <brandt.bowling@gmail.com>
2023-08-07 20:39:04 -0400

    add description field

--- frontend/src/components/shared/card.js
+++ frontend/src/components/shared/card.js
@@ -4,13 +4,13 @@ import mapPinLogo from "../../images/icons/map-pin.svg";
 
 const Card = ({ type, date, name, location, description }) => {
   return (
-    <div className="flex flex-col items-center gap-2 min-w-[9.75rem] min-h-[12.5rem] pb-[2.19rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden md:min-w-[23.75rem] md:min-h-[30rem] md:pb-[3.13rem] md:gap-5">
+    <div className="flex flex-col items-center gap-2 w-[9.75rem] h-[12.5rem] pb-[2.19rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden md:w-[23.75rem] md:h-[30rem] md:pb-[3.13rem] md:gap-5">
       <img
         className="h-[6.15788rem] w-[10.94738rem] flex-shrink-0 mb-0 md:h-60 md:w-[26.66669rem]"
         src={prayerGatheringImg}
         alt="Prayer Gathering"
       />
-      <div className="flex flex-col gap-1 items-start min-w-[8.75rem] min-h-[3.63rem] md:gap-3 md:min-w-[20rem]">
+      <div className="flex flex-col gap-1 items-start w-[8.75rem] md:gap-3 md:w-[20rem]">
         <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           Th, May 25, 2023
         </p>
@@ -23,6 +23,11 @@ const Card = ({ type, date, name, location, description }) => {
             T-Center
           </p>
         </div>
+        <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
+          I&apos;m baby migas fam yuccie, big mood freegan affogato everyday
+          carry hashtag four dollar toast truffaut 3 wolf moon beard. Hella
+          live-edge brunch neutral milk hotel pabst.
+        </p>
       </div>
     </div>
   );
