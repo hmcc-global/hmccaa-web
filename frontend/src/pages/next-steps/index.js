79334d7e089b288acaeb82e2326c955f063b758a
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2023-08-07 22:52:37 -0400

    updates for ministry next steps

--- frontend/src/pages/next-steps/index.js
+++ frontend/src/pages/next-steps/index.js
@@ -5,6 +5,7 @@ import Seo from "../../components/seo";
 import LifeGroup from "../../components/page-next-steps/lifeGroup";
 import MissionsNextStep from "../../components/page-next-steps/missionsNextSteps";
 import Banner from "../../components/shared/banner";
+import MinistryTeams from "../../components/page-next-steps/ministryTeams";
 
 const NextStepsPage = () => (
   <Layout>
@@ -15,7 +16,9 @@ const NextStepsPage = () => (
         <div>Membership section</div>
         <div>Transformation classes section</div>
         <div>Baptism section</div>
-        <div>Ministry teams section</div>
+      </div>
+      <MinistryTeams />
+      <div className="max-w-container mx-auto">
         <MissionsNextStep />
       </div>
     </div>
