ab505a86689b5f18f77dd44b198ed1482f366bb4
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2023-08-07 22:53:57 -0400

    format files

--- frontend/src/components/page-next-steps/ministryTeams.js
+++ frontend/src/components/page-next-steps/ministryTeams.js
@@ -7,7 +7,15 @@ import {
   teamWrapper,
   wrapper,
 } from "../../css/ministryTeams.module.css";
-import { AdsLogo, KidsLogo, DocsLogo, HostsLogo, FacilitiesLogo, VMLogo, WorshipLogo } from "../svgs";
+import {
+  AdsLogo,
+  KidsLogo,
+  DocsLogo,
+  HostsLogo,
+  FacilitiesLogo,
+  VMLogo,
+  WorshipLogo,
+} from "../svgs";
 
 const teams = [
   {
@@ -62,55 +70,58 @@ const MinistryTeams = () => (
         </p>
       </div>
       <div className="flex flex-col gap-y-3 lg:block py-5 lg:py-0">
-        {
-            teams.map((item, index) => {
-                const Component = item.Component
-                const key = `team-${index + 1}`;
-                const teamItem = item.team.charAt(0);
-                let className = "";
-                switch (teamItem) {
-                    case "A": 
-                        className = "lg:top-[1.8125rem] lg:left-[0.4375rem]";
-                        break;
-                    case "B": 
-                        className = "lg:top-[12.5375rem] xl:top-[19.9375rem] lg:-right-[3.775rem] xl:-right-[14.875rem]";
-                        break;
-                    case "D":
-                        className = "lg:top-[1.875rem] lg:-right-[1.25rem]";
-                        break;
-                    case "H": 
-                        className = "lg:top-[39.0625rem] lg:-right-[2.6875rem] xl:-right-[9.6875rem]"
-                        break;
-                    case "S":
-                        className  = "lg:-bottom-[2.8125rem] lg:left-[17.1875rem]";
-                        break;
-                    case "V":
-                        className = "lg:bottom-[9.6875rem] lg:-left-[3.875rem] xl:-left-[4.875rem]";
-                        break;
-                    case "W":
-                        className = "lg:top-[12.5375rem] xl:top-[19.9375rem] lg:-left-[3.85rem] xl:-left-[11.25rem]";
-                        break;
-                    default:
-                }
-                return (
-                    <div key={key} className={`${ministryTeam} ${className}`}>
-                    <div className={teamWrapper}>
-                      <div>
-                        <Component />
-                      </div>
-                      <div>
-                        <h3>{item.team}</h3>
-                        <div>({item.abbreviation})</div>
-                      </div>
-                    </div>
-                  </div>
-            )
+        {teams.map((item, index) => {
+          const Component = item.Component;
+          const key = `team-${index + 1}`;
+          const teamItem = item.team.charAt(0);
+          let className = "";
+          switch (teamItem) {
+            case "A":
+              className = "lg:top-[1.8125rem] lg:left-[0.4375rem]";
+              break;
+            case "B":
+              className =
+                "lg:top-[12.5375rem] xl:top-[19.9375rem] lg:-right-[3.775rem] xl:-right-[14.875rem]";
+              break;
+            case "D":
+              className = "lg:top-[1.875rem] lg:-right-[1.25rem]";
+              break;
+            case "H":
+              className =
+                "lg:top-[39.0625rem] lg:-right-[2.6875rem] xl:-right-[9.6875rem]";
+              break;
+            case "S":
+              className = "lg:-bottom-[2.8125rem] lg:left-[17.1875rem]";
+              break;
+            case "V":
+              className =
+                "lg:bottom-[9.6875rem] lg:-left-[3.875rem] xl:-left-[4.875rem]";
+              break;
+            case "W":
+              className =
+                "lg:top-[12.5375rem] xl:top-[19.9375rem] lg:-left-[3.85rem] xl:-left-[11.25rem]";
+              break;
+            default:
+          }
+          return (
+            <div key={key} className={`${ministryTeam} ${className}`}>
+              <div className={teamWrapper}>
+                <div>
+                  <Component />
+                </div>
+                <div>
+                  <h3>{item.team}</h3>
+                  <div>({item.abbreviation})</div>
+                </div>
+              </div>
+            </div>
+          );
         })}
       </div>
       <div className={highlightText}>
-          Please email <a href="mailto:abc@hmccaa.com">abc@hmccaa.com</a> to get
-          connected!
-        </div>
+        Please email <a href="mailto:abc@hmccaa.com">abc@hmccaa.com</a> to get
+        connected!
+      </div>
     </div>
   </div>
 );
