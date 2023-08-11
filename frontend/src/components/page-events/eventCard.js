a295069c465a95e0c116cf96d8e444eb8584231f
Brandt Bowling <brandt.bowling@gmail.com>
2023-08-10 21:04:58 -0400

    Use StaticImage
    Co-authored-by: thedomcab <9808478+thedomcab@users.noreply.github.com>

--- frontend/src/components/page-events/eventCard.js
+++ frontend/src/components/page-events/eventCard.js
@@ -7,7 +7,7 @@ const EventCard = ({ title, date, location, img, description }) => {
   const attributes = (
     <div>
       <div className="flex items-center gap-1">
-        <img className="w-4 h-4 mb-0" src={mapPinLogo} alt="Map" />
+        <StaticImage className="w-4 h-4 mb-0" src="../../images/icons/map-ping.svg" alt="Map" />
         <p className="mb-0 text-black text-[0.625rem] leading-[0.9375rem] font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           {location}
         </p>
