1d079d0b16c8a7b9ba28308f4ef5fd92ef5ca490
graceschae <gchae@umich.edu>
2023-08-12 11:27:58 -0400

    pretty js

--- frontend/src/components/page-events/eventCard.js
+++ frontend/src/components/page-events/eventCard.js
@@ -6,7 +6,11 @@ const EventCard = ({ title, date, location, img, description }) => {
   const attributes = (
     <div>
       <div className="flex items-center gap-1">
-        <StaticImage className="w-4 h-4 mb-0" src="../../images/icons/map-pin.svg" alt="Map" />
+        <StaticImage
+          className="w-4 h-4 mb-0"
+          src="../../images/icons/map-pin.svg"
+          alt="Map"
+        />
         <p className="mb-0 text-black text-[0.625rem] leading-[0.9375rem] font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           {location}
         </p>
@@ -22,8 +26,9 @@ const EventCard = ({ title, date, location, img, description }) => {
       title={title}
       date={date}
       img={img} // TODO: make dynamic
-     
-    >{attributes}</Card>
+    >
+      {attributes}
+    </Card>
   );
 };
 
--- frontend/src/components/shared/card.js
+++ frontend/src/components/shared/card.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const Card = ({ img, date, title, children }) => {
   return (
     <div className="flex flex-col items-center gap-2 w-[9.75rem] h-[12.5rem] pb-[2.19rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden md:w-[23.75rem] md:h-[30rem] md:pb-[3.13rem] md:gap-5">
-   {img}
+      {img}
       <div className="flex flex-col gap-1 items-start w-[8.75rem] md:gap-3 md:w-[20rem]">
         <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           {date}
