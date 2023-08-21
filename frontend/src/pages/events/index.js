ec5d8fb36332593200893721206caf924675f15c
Joe Yang <josephjyang@gmail.com>
2023-08-20 18:05:45 -0700

    add events map

--- frontend/src/pages/events/index.js
+++ frontend/src/pages/events/index.js
@@ -8,30 +8,42 @@ import { StaticImage } from "gatsby-plugin-image";
 import EventCard from "../../components/page-events/eventCard";
 import Banner from "../../components/shared/banner";
 
-const EventsPage = () => (
-  <Layout>
-    <Banner bgImage="bg-center bg-events">Upcoming Events</Banner>
-    <SundayCelebBarEvents />
-    <div>Event list section</div>
-    <EventCard
-      title="Prayer Gathering"
-      date="Th, May 25, 2023"
-      img={
-        <StaticImage
-          className="h-[6.15788rem] w-[10.94738rem] flex-shrink-0 mb-0 md:h-60 md:w-[26.66669rem]"
-          src="../../images/prayer-gathering.png"
-          alt="Background"
-        />
-      }
-      location="T Center"
-      description="I'm baby migas fam yuccie, big mood freegan affogato everyday
-          carry hashtag four dollar toast truffaut 3 wolf moon beard. Hella
-          live-edge brunch neutral milk hotel pabst."
-    />
-    <div>Prayer Gatherings bar</div>
-    <Instagram />
-  </Layout>
-);
+const EventsPage = () => {
+  const events = [{
+    id: 1,
+    title: "HMCC Event",
+    date: "Th, May 25, 2023",
+    imgUrl: "../../images/prayer-gathering.png",
+    imgAlt: "example",
+    location: "TCenter",
+    description: "event at Hmcc"
+  }] // Todo: fetch actual events from backend
+
+  return (
+    <Layout>
+      <SundayCelebBarEvents />
+      <div>Event list section</div>
+          {events.map(event => (
+            <EventCard
+              key={event.id}
+              title={event.title}
+              date={event.date}
+              img={
+                <StaticImage
+                  className="h-[6.15788rem] w-[10.94738rem] flex-shrink-0 mb-0 md:h-60 md:w-[26.66669rem]"
+                  src={event.imgUrl}
+                  alt={event.imgAlt}
+                />
+              }
+              location={event.location}
+              description={event.description}
+            />
+          ))}
+      <div>Prayer Gatherings bar</div>
+      <Instagram />
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Events" />;
 
