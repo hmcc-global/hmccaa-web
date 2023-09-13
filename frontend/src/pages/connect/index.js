45a4e51b8cb2af38ab2428b0af6d12a0d4a62af5
Jason Qi <jasonzq@umich.edu>
2023-09-12 22:05:01 -0400

    Sunday Celebration Progress

--- frontend/src/pages/connect/index.js
+++ frontend/src/pages/connect/index.js
@@ -5,15 +5,18 @@ import Seo from "../../components/seo";
 import Questions from "../../components/gotQuestions";
 import Instagram from "../../components/instaBar";
 import Banner from "../../components/shared/banner";
+import SundayCelebration from "../../components/page-connect/sunday-celebration"
 
 const ConnectPage = () => (
   <Layout>
-    <Banner bgImage="bg-connect bg-center">Connect</Banner>
-    <div>Sunday Celebration section</div>
-    <div>Common Questions section</div>
-    <div>Life Stages section</div>
-    <Questions />
-    <Instagram />
+    <Banner bgImage="bg-connect bg-[center_top]">Connect</Banner>
+    <div className="max-w-container">
+      <SundayCelebration />
+      <div>Common Questions section</div>
+      <div>Life Stages section</div>
+      <Questions />
+      <Instagram />
+    </div>
   </Layout>
 );
 
