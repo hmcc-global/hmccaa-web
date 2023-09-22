082bda7922a6f4047a69b4ad7c485ef1ddb6f35e
yijo <yijo@umich.edu>
2023-09-22 16:20:00 -0400

    things

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -4,44 +4,34 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { PrimaryButtonLink, SecondaryButtonLink} from "../components/Button";
 import { container,titleContainer } from "../css/404.module.css";
-import { HighlightedParagraph } from "../components/shared/highlightedParagraph";
 import ErrorImage from "../images/404placeholder.png";
 
 const NotFoundPage = () => (
   <Layout>
     <div className={container}>
-      <div className="flex pb-20 mb-80">
-        <div className="basis-1/3 pr-8 pt-10 pb-20 order-1">
-          <h1 className="mb-20">404: Error</h1>
+      <div className="justify-center flex pb-20 mb-80 xl:flex-row lg:flex-row md:flex-row sm:flex-row flex-col">
+        <div className="flex justify-center basis-1/3 pr-8 pt-10 pb-20 order-1">
+          <h1 className="relative mb-20">404: Error</h1>
           <div>
-            <img className="object-contain h-70 w-150" src={ErrorImage} alt="404 Error" />
+            <img className="object-contain w-full h-full" src={ErrorImage} alt="404 Error" />
           </div>
         </div>
-        <div className="pl-2.5 pr-8 pt-12 order-2 mt-20 ml-14">
+        <div className="bg-Primary-500 pl-2.5 pr-8 pt-12 order-2 mt-20 ml-14">
           <oops>OOPS!</oops>
           <h4 className="pb-20">WE COULDN'T FIND THAT PAGE</h4>
           <div className="font-medium pb-6">Try going back or clicking one of the links below:</div>
           <div className="pb-3">
-            <a
-              key={`browseLink-sundayCelebrationInfo`}
-              href={"/"}
-            >
+            <a key={`browseLink-sundayCelebrationInfo`} href={"/"}>
               <link1>Sunday Celebration Info</link1>
             </a>
           </div>
           <div className="pb-3">
-            <a
-              key={`browseLink-lifegroupSignup`}
-              href={"/"}
-            >
+            <a key={`browseLink-lifegroupSignup`} href={"/"}>
               <link1>Life Group Sign Up</link1>
             </a>
           </div>
           <div className="pb-3">
-            <a
-              key={`browseLink-upcomingEvents`}
-              href={"/"}
-            >
+            <a key={`browseLink-upcomingEvents`} href={"/"}>
               <link1>Upcoming Events</link1>
             </a>
           </div>
