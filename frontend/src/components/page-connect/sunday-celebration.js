98e03c0b8a98a2b67a086301234d5763cb6afd43
Jason Qi <jasonzq@umich.edu>
2023-11-20 21:58:44 -0500

    Updates to Sunday Celebration Section

--- frontend/src/components/page-connect/sunday-celebration.js
+++ frontend/src/components/page-connect/sunday-celebration.js
@@ -1,29 +1,21 @@
 import * as React from "react"
-import imgGreeters from "../../images/connect-greeters.png"
 import { StaticImage } from "gatsby-plugin-image"
-import {
-  statementContainer,
-  globalMissionContainer,
-  globalMission,
-} from "../../css/partners.module.css"
-import {container} from "../../css/belief.module.css"
-import { statement } from "../../css/sundayCelebration.module.css";
+import { HighlightedParagraph } from "../shared/highlightedParagraph";
 
 
 const sundayCelebration = () => (
-  <div className={container}>
-    <div className={globalMissionContainer}>
-      <div className="pb-[20px]">
-        <StaticImage src="../../images/connect-greeters.png" alt="Greeters"/>
+  <div className="px-4 pt-[0.9375rem] pb-[3.125rem] lg:py-[3.75rem] flex flex-col items-center lg:items-start justify-center lg:flex-row lg:gap-[60px]">
+    <div className="flex-shrink-0 lg:order-2 pb-5 lg:pb-0">
+        <StaticImage src="../../images/connect-greeters.png" alt="Greeters" className="mb-0"/>
       </div>
-      <div className={globalMission}><h3>Greeters (host team welcoming)</h3></div>
-    </div>
-    <div className={statementContainer}>
-      <div className="subheading">SUNDAY CELEBRATION</div>
-        <h2>COME CELEBRATE WITH US!</h2>
-        <div className={statement}>
-          <p>Sunday Celebration is HMCC’s weekend gathering where we come together to worship God and celebrate His work in our lives. All are welcome!</p>
-        </div>
+    <div className="text-center lg:basis-[49.32%] lg:order-1 lg:items-center lg:text-left">
+      <div className="flex flex-col gap-3">
+        <div className="subheading">SUNDAY CELEBRATION</div>
+          <h2 className="pb-5">COME CELEBRATE WITH US!</h2>
+          <HighlightedParagraph>
+            <p>Sunday Celebration is HMCC’s weekend gathering where we come together to worship God and celebrate His work in our lives. All are welcome!</p>
+          </HighlightedParagraph>
+      </div>
     </div>
 </div> 
 );
