dfda606a2da6fc6fa941209044690ebc268b0621
Maple Xu <maplexu2010@gmail.com>
2023-11-28 21:13:30 -0500

    trying more stuff

--- frontend/src/components/page-watch/index/sermons.js
+++ frontend/src/components/page-watch/index/sermons.js
@@ -1,34 +1,35 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Sermons = () => {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      allStrapiSermon {
-        nodes {
-          Title
-          VideoLink
-          Series {
-            Name
-            id
-          }
-          Preacher {
-            Name
-            Prefix
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query MyQuery {
+  //     allStrapiSermon {
+  //       nodes {
+  //         Title
+  //         VideoLink
+  //         Series {
+  //           Name
+  //           id
+  //         }
+  //         Preacher {
+  //           Name
+  //           Prefix
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
   return (
-    <div>
-      {data.allStrapiSermon.nodes.map((sermon, i) => (
-        <div key={i}>
-          {sermon.Title} {sermon.VideoLink} {sermon.Series.Name}{" "}
-          {sermon.Preacher.Prefix} {sermon.Preacher.Name}
-        </div>
-      ))}
-    </div>
-  )
-}
-export default Sermons
+    // <div>
+    //   {data.allStrapiSermon.nodes.map((sermon, i) => (
+    //     <div key={i}>
+    //       {sermon.Title} {sermon.VideoLink} {sermon.Series.Name}{" "}
+    //       {sermon.Preacher.Prefix} {sermon.Preacher.Name}
+    //     </div>
+    //   ))}
+    // </div>
+    <div>hello</div>
+  );
+};
+export default Sermons;
