db4ad76a1504d42afe29837aad18d8ee96dc00f9
Maple Xu <mapxu@microsoft.com>
2023-11-29 17:36:20 +0000

    strapi+gatsby working

--- frontend/src/components/page-watch/index/sermons.js
+++ frontend/src/components/page-watch/index/sermons.js
@@ -2,34 +2,33 @@ import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 const Sermons = () => {
-  // const data = useStaticQuery(graphql`
-  //   query MyQuery {
-  //     allStrapiSermon {
-  //       nodes {
-  //         Title
-  //         VideoLink
-  //         Series {
-  //           Name
-  //           id
-  //         }
-  //         Preacher {
-  //           Name
-  //           Prefix
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allStrapiSermon {
+        nodes {
+          Title
+          VideoLink
+          Series {
+            Name
+            id
+          }
+          Preacher {
+            Name
+            Prefix
+          }
+        }
+      }
+    }
+  `);
   return (
-    // <div>
-    //   {data.allStrapiSermon.nodes.map((sermon, i) => (
-    //     <div key={i}>
-    //       {sermon.Title} {sermon.VideoLink} {sermon.Series.Name}{" "}
-    //       {sermon.Preacher.Prefix} {sermon.Preacher.Name}
-    //     </div>
-    //   ))}
-    // </div>
-    <div>hello</div>
+    <div>
+      {data.allStrapiSermon.nodes.map((sermon, i) => (
+        <div key={i}>
+          {sermon.Title} {sermon.VideoLink} {sermon.Series.Name}{" "}
+          {sermon.Preacher.Prefix} {sermon.Preacher.Name}
+        </div>
+      ))}
+    </div>
   );
 };
 export default Sermons;
