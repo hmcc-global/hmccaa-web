f6d196e28f91e7df2d62b8193a933f1cf6a217e4
Justin Jaesung Lee <justinjl@umich.edu>
2023-12-01 21:14:52 -0500

    Using tailwind instead of inline styles

--- frontend/src/components/page-main/getconnected.js
+++ frontend/src/components/page-main/getconnected.js
@@ -3,7 +3,7 @@ import { SecondaryButtonLink } from "../Button";
 import gc94 from "../../images/gc94.svg"
 
 const GetConnectedText = () => (
-        <div style={{ textAlign: "center" }} className="w-full flex-col justify-center">
+        <div className="w-full flex-col justify-center text-center">
           <div className="subheading">Get Connected</div>
           <h2>
             There is a place for you at HMCC.
@@ -21,12 +21,12 @@ const GetConnectedText = () => (
 );
 
 const GetConnectedCircle = () => (
-  <div style={{ display: "flex", flexDirection: "column", position: "relative" }} className="h-full flex-col">
+  <div className="h-full flex-col relative">
     <img
       alt="Images of LIFE Groups arranged in a circle"
       src={gc94}
     />
-    <div style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -75%)" }}>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-3/4">
       <GetConnectedText />
     </div>
   </div>
