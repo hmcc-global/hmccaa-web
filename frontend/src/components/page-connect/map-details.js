ae3af7a589fd9a604ec7460243b87c71d86797ba
bbowling <brandt.bowling@gmail.com>
2023-12-04 20:51:04 -0500

    styling to match format

--- frontend/src/components/page-connect/map-details.js
+++ frontend/src/components/page-connect/map-details.js
@@ -3,18 +3,23 @@ import * as React from "react";
 const MapDetails = () => {
   return (
     <div className="px-17 py-5 h-60 bg-Neutral-200">
-      <div className="w-[14.25rem] h-[12.6875] gap-5 flex flex-col justify-center items-center text-center ">
-        <p className=" font-raleway text-lg font-medium leading-5">
-          Sunday Mornings 10 AM
+      <div className="w-[14.25rem] h-[12.6875] gap-5 flex flex-col justify-center items-center text-center">
+        <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0">
+          Sunday Mornings <br />{" "}
+          <span className="font-bold text-xl">10 AM</span>
         </p>
-        <p className="w-[11.8125rem] h-[5.735rem]">
-          <span className="font-medium text-lg">Transformation Center</span>
-          <br />{" "}
-          <span className="font-bold text-xl">
-            1001 E Huron St, <br /> Ann Arbor, MI 48104
-          </span>
-        </p>
-        <p>MAPS & DIRECTIONS</p>
+        <div className="gap-1">
+          <p className="mb-0">
+            <span className="font-medium text-lg">Transformation Center</span>
+            <br />{" "}
+            <span className="font-bold text-xl">
+              1001 E Huron St, <br /> Ann Arbor, MI 48104
+            </span>
+          </p>
+          <div className="pt-3 pb-3 pl-5 pr-5 text-Accent-500 underline font-bold leading-6 text-lg tracking-medium-wide">
+            MAPS & DIRECTIONS
+          </div>
+        </div>
       </div>
     </div>
   );
