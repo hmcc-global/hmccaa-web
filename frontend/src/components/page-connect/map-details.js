d1d5910ce2c18184825cdc7c87a1d022075fe449
Brandt Bowling <brandt.bowling@gmail.com>
2023-12-06 22:34:24 -0500

    Add button link. fix formatting

--- frontend/src/components/page-connect/map-details.js
+++ frontend/src/components/page-connect/map-details.js
@@ -1,12 +1,13 @@
 import * as React from "react";
+import { SecondaryButtonLink } from "../Button";
 
 const MapDetails = () => {
   return (
-    <div className="px-17 py-5 h-60 bg-Neutral-200">
+    <div className="flex items-center justify-center px-[3.66rem] py-5 h-[15.6875rem] bg-Neutral-200">
       <div className="w-[14.25rem] h-[12.6875] gap-5 flex flex-col justify-center items-center text-center">
-        <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0">
+        <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0 text-Shades-100">
           Sunday Mornings <br />{" "}
-          <span className="font-bold text-xl">10 AM</span>
+          <span className="font-bold text-xl text-Shades-100">10 AM</span>
         </p>
         <div className="gap-1">
           <p className="mb-0">
@@ -16,9 +17,12 @@ const MapDetails = () => {
               1001 E Huron St, <br /> Ann Arbor, MI 48104
             </span>
           </p>
-          <div className="pt-3 pb-3 pl-5 pr-5 text-Accent-500 underline font-bold leading-6 text-lg tracking-medium-wide">
+          <SecondaryButtonLink hasArrow={true} className={"bg-Shades-0"}>
             MAPS & DIRECTIONS
-          </div>
+          </SecondaryButtonLink>
+          {/* <div className="pt-3 pb-3 pl-5 pr-5 text-Accent-500 underline font-bold leading-6 text-lg tracking-medium-wide">
+            MAPS & DIRECTIONS
+          </div> */}
         </div>
       </div>
     </div>
