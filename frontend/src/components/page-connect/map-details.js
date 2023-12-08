682839ab76e39fae53baed7120f1f8fb7934305e
bbowling <brandt.bowling@gmail.com>
2023-12-07 23:29:37 -0500

    fix image

--- frontend/src/components/page-connect/map-details.js
+++ frontend/src/components/page-connect/map-details.js
@@ -4,29 +4,31 @@ import { SecondaryButtonLink } from "../Button";
 
 const MapDetails = () => {
   return (
-    <div className="flex flex-col items-center justify-center px-[3.66rem] py-5 h-[15.6875rem] bg-Neutral-200">
-      <div className="w-[14.25rem] h-[12.6875] gap-5 flex flex-col justify-center items-center text-center">
-        <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0 text-Shades-100">
-          Sunday Mornings <br />{" "}
-          <span className="font-bold text-xl text-Shades-100">10 AM</span>
-        </p>
-        <div className="gap-1">
-          <p className="mb-0">
-            <span className="font-medium text-lg">Transformation Center</span>
-            <br />{" "}
-            <span className="font-bold text-xl">
-              1001 E Huron St, <br /> Ann Arbor, MI 48104
-            </span>
+    <>
+      <div className="flex flex-col items-center justify-center px-[3.66rem] py-5 h-[15.6875rem] bg-Neutral-200">
+        <div className="w-[14.25rem] h-[12.6875] gap-5 flex flex-col justify-center items-center text-center">
+          <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0 text-Shades-100">
+            Sunday Mornings <br />{" "}
+            <span className="font-bold text-xl text-Shades-100">10 AM</span>
           </p>
-          <SecondaryButtonLink hasArrow={true} className={"bg-Shades-0"}>
-            MAPS & DIRECTIONS
-          </SecondaryButtonLink>
+          <div className="gap-1">
+            <p className="mb-0">
+              <span className="font-medium text-lg">Transformation Center</span>
+              <br />{" "}
+              <span className="font-bold text-xl">
+                1001 E Huron St, <br /> Ann Arbor, MI 48104
+              </span>
+            </p>
+            <SecondaryButtonLink hasArrow={true} className={"bg-Shades-0"}>
+              MAPS & DIRECTIONS
+            </SecondaryButtonLink>
+          </div>
         </div>
       </div>
       <div>
         <StaticImage src="../../images/connect-map-parking.png" />
       </div>
-    </div>
+    </>
   );
 };
 
