78552a1e0b9226aa81126eafcd2943b75d1cab23
jchenggggg <joshcheng36@gmail.com>
2023-12-10 22:12:25 -0500

    Add Welcome and Sunday Celebration sections to the I'm New page (#35)
    Co-authored-by: Josh Cheng <jcheng@duosecurity.com>
    Co-authored-by: Brandt Bowling <brandt.bowling@gmail.com>

--- frontend/src/pages/new/index.js
+++ frontend/src/pages/new/index.js
@@ -2,13 +2,17 @@ import * as React from "react";
 
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
+import WelcomeSection from "../../components/page-new/welcome";
+import SundayCelebrationSection from "../../components/page-new/sunday-celebration";
 import Questions from "../../components/gotQuestions";
 import SundayCelebBarNew from "../../components/page-new/sundayCelebBarNew";
 import Banner from "../../components/shared/banner";
+
 const NewcomersPage = () => (
   <Layout>
     <Banner bgImage="bg-center bg-new">I&apos;m New</Banner>
-    <div>Welcome section</div>
+    <WelcomeSection/>
+    <SundayCelebrationSection/>
     <SundayCelebBarNew />
     <div>Life Stages</div>
     <div className="pl-[21px] pr-[11px] lg:px-4 w-full flex justify-center">
