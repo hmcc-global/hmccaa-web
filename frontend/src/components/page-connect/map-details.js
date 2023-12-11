af779929abdd87a6635d356d794038ce45194a82
bbowling <brandt.bowling@gmail.com>
2023-12-10 20:30:31 -0500

    remove height/widths

--- frontend/src/components/page-connect/map-details.js
+++ frontend/src/components/page-connect/map-details.js
@@ -4,10 +4,10 @@ import { SecondaryButtonLink } from "../Button";
 
 const MapDetails = () => {
   return (
-    <div className="flex flex-col items-center justify-center pt-5 bg-Neutral-200 md:flex-row md:h-[30.25rem] md:px-[8.125rem] md:flex-shrink-0 md:bg-[#fff]">
-      <div className=" h-[12.6875] px-[3.66rem] gap-5 md:h-[26.25rem] md:rounded-l-[1.25rem] md:w-[23.75rem] md:px-25  md:bg-Neutral-200 ">
-        <div className="flex flex-col justify-center items-center text-center md:gap-8 md:py-[4.75rem] md:px-20">
-          <p className="w-[8.125rem] h-[2.8125rem] text-lg font-medium leading-tighter mb-0 text-Shades-100">
+    <div className="flex flex-col justify-center pt-5 bg-Neutral-200 md:flex-row md:px-[8.125rem] md:pb-5 md:flex-shrink-0 md:bg-[#fff]">
+      <div className="px-[3.66rem] gap-5 md:rounded-l-[1.25rem] md:bg-Neutral-200 ">
+        <div className="flex flex-col justify-center items-center text-center md:gap-8 md:py-[4.75rem]">
+          <p className="text-lg font-medium leading-tighter mb-0 text-Shades-100">
             Sunday Mornings <br />{" "}
             <span className="font-bold text-xl text-Shades-100">10 AM</span>
           </p>
@@ -29,7 +29,7 @@ const MapDetails = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center bg-Neutral-200 md:h-[26.25rem] md:rounded-r-[1.25rem]">
+      <div className="flex justify-center md:rounded-r-[1.25rem]">
         <StaticImage src="../../images/connect-map-parking.png" alt="" />
       </div>
     </div>
