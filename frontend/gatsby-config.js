96c3cf37ed85922fd2ad9b448469b8a7a803fa84
xumaple <45406854+xumaple@users.noreply.github.com>
2023-12-17 14:03:18 -0500

    Fix dev pipeline (#94)
    * update pipeline with env file
    
    * fix
    
    * remove this branch
    
    ---------
    
    Co-authored-by: Maple Xu <mapxu@microsoft.com>

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
+        apiURL: process.env.STRAPI_API_URL,
         accessToken: process.env.STRAPI_TOKEN,
         collectionTypes: ["sermon"],
         singleTypes: [],
