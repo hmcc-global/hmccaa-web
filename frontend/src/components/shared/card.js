d654e40221eba3ae6addfd9cb9bf54f518995b24
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2023-12-19 16:08:14 -0500

    fix cards for responsive and match figma mocks

--- frontend/src/components/shared/card.js
+++ frontend/src/components/shared/card.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-const Card = ({ img, date, title, children }) => {
+const Card = ({ img, date, title, children, containerCss }) => {
   return (
-    <div className="flex flex-col items-center gap-2 w-[9.75rem] h-[12.5rem] pb-[2.19rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden md:w-[23.75rem] md:h-[30rem] md:pb-[3.13rem] md:gap-5">
+    <div
+      className={`flex flex-col items-center gap-2 pb-[0.9375rem] mx-auto rounded-xl border border-solid border-b-Neutral-900 shadow-md overflow-hidden ${containerCss} md:gap-5`}
+    >
       {img}
-      <div className="flex flex-col gap-1 items-start w-[8.75rem] md:gap-3 md:w-[20rem]">
+      <div className="flex flex-col gap-1 items-start md:gap-3 px-2 w-full md:px-[1.8125rem]">
         <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
           {date}
         </p>
