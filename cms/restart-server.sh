56d744c0da48836d0baafe970804824f8f2548ad
xumaple <45406854+xumaple@users.noreply.github.com>
2023-12-21 10:49:29 -0500

    Set up build tag for latest build to prep for cron job (#106)
    * set up build tag for latest build to prep for cron job
    
    * rm this branch
    
    * update pipeline to only run if files are changed
    
    * add milliseconds
    
    ---------
    
    Co-authored-by: Maple Xu <mapxu@microsoft.com>

--- cms/restart-server.sh
+++ cms/restart-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This file is used for restarting strapi.
-# It is run by the server's cron job once a day.
+# It is run by the server's cron job whenever a new change is pushed.
 
 NODE_ENV=production npm run build
 pm2 restart strapi
