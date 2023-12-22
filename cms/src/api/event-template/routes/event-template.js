bbbee923b41320a0c7792062b9d9fece5a84e9b0
Maple Xu <mapxu@microsoft.com>
2023-12-22 18:06:53 +0000

    events api v2

---
+++ cms/src/api/event-template/routes/event-template.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-template router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::event-template.event-template');
---
+++ cms/src/api/event-template/services/event-template.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-template service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::event-template.event-template');
