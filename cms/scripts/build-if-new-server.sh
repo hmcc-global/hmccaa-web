55dd743016befc7f40bb4b0de9dceb8a3dd25053
Maple Xu <mapxu@microsoft.com>
2023-12-22 18:51:20 +0000

    log builds

--- cms/scripts/build-if-new-server.sh
+++ cms/scripts/build-if-new-server.sh
@@ -5,13 +5,16 @@
 #
 # Lives in `<repo-root>/cron`.
 
+# Logs to log_file declared below
+# To see crontab logs, run:
+#     sudo cat /var/log/syslog | grep -w 'cron'
+log_file="$HOME/annarbor-content/cron/build.log"
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
-
-log_file="$HOME/annarbor-content/cron/build.log"
 
 print_info() {
     time=`date '+%Y-%m-%d %T.%3N'`
@@ -26,7 +29,8 @@ latest_build_old="$HOME/annarbor-content/cron/latest-build_old"
 if ! cmp -s $latest_build $latest_build_old; then
     print_info "Found newer build. Restarting server..."
     cp $latest_build $latest_build_old
-    sh $HOME/annarbor-content/scripts/restart-server.sh && print_info "Build succeeded." || print_info "Build failed."
+    build_log_file="$HOME/build-`date '+%Y-%m-%d%T-%3N'`.log"
+    sh $HOME/annarbor-content/scripts/restart-server.sh > $build_log_file && print_info "Build succeeded." || print_info "Build failed."
 else
     print_info "Build has not changed."
 fi
