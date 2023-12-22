d2abd02c1847e5e4fe579f39310bc1ecad3c0072
Maple Xu <mapxu@microsoft.com>
2023-12-22 21:29:53 +0000

    export nvm

--- cms/scripts/restart-server.sh
+++ cms/scripts/restart-server.sh
@@ -3,5 +3,10 @@
 # This file is used for restarting strapi.
 # It is run by the server's cron job whenever a new change is pushed.
 
+# Load nvm if it doesn't exist
+[ -z "${NVM_DIR}" ] || export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Restart server
 NODE_ENV=production npm run build
 pm2 restart strapi
