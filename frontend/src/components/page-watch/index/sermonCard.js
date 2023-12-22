27e51d3ed82599acf51e818bd18ced0faa86627b
Brandt Bowling <brandt.bowling@gmail.com>
2023-12-22 13:57:34 -0500

    Update frontend/src/components/page-watch/index/sermonCard.js
    Co-authored-by: thedomcab <9808478+thedomcab@users.noreply.github.com>

--- frontend/src/components/page-watch/index/sermonCard.js
+++ frontend/src/components/page-watch/index/sermonCard.js
@@ -3,8 +3,8 @@ import Card from "../../shared/card";
 
 const SermonCard = ({ img, date, title, speaker, passage, series }) => {
   const attributes = (
-    <div className="pt-5">
-      <p className="leading-6 font-normal text-lg">
+    <div className="pt-1 md:pt-2">
+      <p className="leading-6 font-normal text-lg mb-0">
         Speaker: {speaker}
         <br />
         Passage: {passage}
