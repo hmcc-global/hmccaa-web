e22bc9b2d5e7c5847ed478930d6101b2cf034551
terrianx <txiaopk@gmail.com>
2023-12-23 15:35:01 -0500

    Small whitespace fix

--- frontend/src/components/Link.js
+++ frontend/src/components/Link.js
@@ -1,29 +1,27 @@
 import * as React from "react";
 import { Link as GatsbyLink} from "gatsby";
 
-const Link = ({ href, to, children }) => {
+const Link = ({ href, to, children, ...rest }) => {
   // external link
   if (href) {
     return (
-      <a 
-        href={href}
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href={href} target="_blank" rel="noreferrer" {...rest}>
         {children}
       </a>
     );
   // internal link
   } else if (to) {
     return (
-      <GatsbyLink to={to}>
+      <GatsbyLink to={to} {...rest}>
         {children}
       </GatsbyLink>
     );
   }
 
   return (
-    <div>{children}</div>
+    <div {...rest}>
+      {children}
+    </div>
   );
 };
 
