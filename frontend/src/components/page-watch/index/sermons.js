4c434cd151cd2b3575c540802299334ce1d8392a
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2023-12-23 18:02:04 -0500

    resolve format issue

--- frontend/src/components/page-watch/index/sermons.js
+++ frontend/src/components/page-watch/index/sermons.js
@@ -80,102 +80,103 @@ const Sermons = ({
   }
 
   return (
-  <div>
-    <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-8 py-36 max-w-container px-4">
-      {data.allStrapiSermon.nodes.map((sermon, i) => (
-        <SermonCard
-          key={i}
-          title={sermon.Title}
-          date={sermon.DatePreached}
-          img={
-            <StaticImage
-              src="../../../images/Sermon-Ad-Selah-April.png"
-              alt="Selah"
-            />
-          }
-          speaker={sermon.Preacher.Prefix + " " + sermon.Preacher.Name}
-          passage={sermon.Title}
-          series={sermon.Series.Name}
-        />
-      ))}
-    </div>
-    <div className="flex flex-col items-center">
-      <div className="flex w-full justify-center gap-4">
-        <span>Filters</span>
-        <select className="border-Shades-100 border-[1px]">
-          <option key="speaker-0">Speaker</option>
-          {speakers.map(({ label, value }, index) => (
-            <option key={`speaker-${index + 1}`} value={value}>
-              {label}
-            </option>
-          ))}
-        </select>
-        <select className="border-Shades-100 border-[1px]">
-          <option key="series-0">Series</option>
-          {series.map(({ label, value }, index) => (
-            <option key={`series-${index + 1}`} value={value}>
-              {label}
-            </option>
-          ))}
-        </select>
-        <select className="border-Shades-100 border-[1px]">
-          <option key="book-0">Books</option>
-          {books.map(({ value, label }, index) => (
-            <option key={`book-${index + 1}`} value={value}>
-              {label}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        {nodes.map(
-          (
-            {
-              Title,
-              VideoLink,
-              Series: { Name: seriesName },
-              Preacher: { Prefix, Name: preacherName },
-              ServiceType,
-            },
-            index
-          ) => (
-            <div key={index}>
-              {Title} {VideoLink} {seriesName} {Prefix} {preacherName}{" "}
-              {ServiceType}
-            </div>
-          )
-        )}
-      </div>
-      <div className="flex text-3xl text-Shades-100 font-normal justify-between max-w-[22.8125rem] w-full">
-        {!isFirst ? (
-          <Link
-            to={previous}
-            rel="prev"
-            className="font-roboto text-Accent-500 no-underline"
-          >
-            &lt;
-          </Link>
-        ) : (
-          <span className="font-roboto">&nbsp;</span>
-        )}
-        {pages.map((page, index) => (
-          <NumberPaging
-            key={`page-number-${index}`}
-            page={page}
-            currentPage={currentPage}
+    <div>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-8 py-36 max-w-container px-4">
+        {data.allStrapiSermon.nodes.map((sermon, i) => (
+          <SermonCard
+            key={i}
+            title={sermon.Title}
+            date={sermon.DatePreached}
+            img={
+              <StaticImage
+                src="../../../images/Sermon-Ad-Selah-April.png"
+                alt="Selah"
+              />
+            }
+            speaker={sermon.Preacher.Prefix + " " + sermon.Preacher.Name}
+            passage={sermon.Title}
+            series={sermon.Series.Name}
           />
         ))}
-        {!isLast && (
-          <Link
-            to={next}
-            rel="next"
-            className="font-roboto text-Accent-500 no-underline"
-          >
-            &gt;
-          </Link>
-        )}
       </div>
-  </div>
+      <div className="flex flex-col items-center">
+        <div className="flex w-full justify-center gap-4">
+          <span>Filters</span>
+          <select className="border-Shades-100 border-[1px]">
+            <option key="speaker-0">Speaker</option>
+            {speakers.map(({ label, value }, index) => (
+              <option key={`speaker-${index + 1}`} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          <select className="border-Shades-100 border-[1px]">
+            <option key="series-0">Series</option>
+            {series.map(({ label, value }, index) => (
+              <option key={`series-${index + 1}`} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          <select className="border-Shades-100 border-[1px]">
+            <option key="book-0">Books</option>
+            {books.map(({ value, label }, index) => (
+              <option key={`book-${index + 1}`} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {nodes.map(
+            (
+              {
+                Title,
+                VideoLink,
+                Series: { Name: seriesName },
+                Preacher: { Prefix, Name: preacherName },
+                ServiceType,
+              },
+              index
+            ) => (
+              <div key={index}>
+                {Title} {VideoLink} {seriesName} {Prefix} {preacherName}{" "}
+                {ServiceType}
+              </div>
+            )
+          )}
+        </div>
+        <div className="flex text-3xl text-Shades-100 font-normal justify-between max-w-[22.8125rem] w-full">
+          {!isFirst ? (
+            <Link
+              to={previous}
+              rel="prev"
+              className="font-roboto text-Accent-500 no-underline"
+            >
+              &lt;
+            </Link>
+          ) : (
+            <span className="font-roboto">&nbsp;</span>
+          )}
+          {pages.map((page, index) => (
+            <NumberPaging
+              key={`page-number-${index}`}
+              page={page}
+              currentPage={currentPage}
+            />
+          ))}
+          {!isLast && (
+            <Link
+              to={next}
+              rel="next"
+              className="font-roboto text-Accent-500 no-underline"
+            >
+              &gt;
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
