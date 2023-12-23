b4b791375d30039e52b9efe3015b2a3b26080aa9
terrianx <txiaopk@gmail.com>
2023-12-23 15:21:54 -0500

    Added conditional rendering for links

--- frontend/src/components/Link.js
+++ frontend/src/components/Link.js
@@ -1,10 +1,30 @@
 import * as React from "react";
 import { Link as GatsbyLink} from "gatsby";
 
-const Link = ({
+const Link = ({ href, to, children }) => {
+  // external link
+  if (href) {
+    return (
+      <a 
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {children}
+      </a>
+    );
+  // internal link
+  } else if (to) {
+    return (
+      <GatsbyLink to={to}>
+        {children}
+      </GatsbyLink>
+    );
+  }
 
-}) => (
-  <></>
-);
+  return (
+    <div>{children}</div>
+  );
+};
 
 export default Link;
