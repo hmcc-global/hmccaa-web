873deb8c316643b3e9997fc7c0832e084c5bb44e
Robert Kang <65458638+rokanq@users.noreply.github.com>
2023-12-23 15:38:26 -0500

    Remove unnecessary code

--- frontend/src/pages/events/eventPage.js
+++ frontend/src/pages/events/eventPage.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../../components/layout";
 import { PrimaryButtonLink } from "../../components/Button";
@@ -9,7 +8,6 @@ import clockIcon from "../../images/icons/clock-black.svg"
 import { StaticImage } from "gatsby-plugin-image";
 
 import Seo from "../../components/seo";
-
 
 
 const EventPage = () => (
@@ -49,50 +47,9 @@ const EventPage = () => (
             </div>
         </div>
 
-
-        {/* <div className="flex flex-col gap-y-8 gap-x-32 pt-[15px] pb-8 lg:py-14">
-            <div className="text-center lg:text-left">
-                <h2 className="pt-8 pb-4">Campus Missions Week</h2>
-                <div className="flex flex-col gap-14 inline-flex">
-                    <div>
-                        <div className="flex-col justify-center items-start flex">
-                            <div className="justify-start items-center gap-1 inline-flex">
-                                <div className="w-5 h-5 relative" />
-                                <div className="text-black text-sm font-medium leading-tight">August 27, 2023</div>
-                            </div>
-                            <div className="justify-start items-center gap-1 inline-flex">
-                                <div className="w-5 h-5 relative" />
-                                <div className="text-black text-sm font-medium leading-tight">TBD</div>
-                            </div>
-                            <div className="justify-start items-center gap-1 inline-flex">
-                                <div className="w-5 h-5 relative" />
-                                <div className="text-black text-sm font-medium leading-tight">TBD</div>
-                            </div>
-                        </div>
-                        <div className="flex justify-center lg:justify-start py-5">
-                            <PrimaryButtonLink hasArrow={true} to={"../../next-steps/lifegroups"}>
-                                Sign Up
-                            </PrimaryButtonLink>
-                        </div>
-                    </div>
-                    <div className="lg:order-1 text-center">
-                        <StaticImage
-                            src="https://via.placeholder.com/382x215"
-                            alt="CMW"
-                        />
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="flex-col justify-start items-start gap-y-8 flex">
-                <div className="w-96"><span className="text-black text-2xl font-semibold font-['Raleway'] leading-7">Details<br /></span><span className="text-black text-2xl font-bold font-['Ubuntu'] leading-normal tracking-wider"><br /></span><span className="text-black text-base font-normal font-['Raleway'] leading-normal">Come back a week earlier to reach out to new students!<br /><br></br>Global Access students can also sign up for International Campus Reach using the same form.</span></div>
-                <div className="w-96"><span className="text-black text-2xl font-semibold font-['Raleway'] leading-7">Contact<br /></span><span className="text-black text-base font-normal font-['Ubuntu'] leading-normal tracking-wide"><br /></span><span className="text-black text-base font-normal font-['Raleway'] leading-normal">Have a question? Please contact aaaa at aaaa@hmcc.net</span></div>
-            </div>
-        </div> */}
     </Layout>
 );
 
-export const Head = () => <Seo title="404: Not Found" />;
+export const Head = () => <Seo title="Event Page" />;
 
 export default EventPage;
