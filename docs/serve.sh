d1f5185400d4c4a01b1d1140efcfa0afe8dbffad
Maple Xu <mapxu@microsoft.com>
2024-01-16 03:10:27 +0000

    add while loop

--- docs/serve.sh
+++ docs/serve.sh
@@ -5,8 +5,13 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 docker stop docs
 docker run -it --init -dp 4000:4000  -v `pwd`:`pwd` -w `pwd` --name docs --rm $(docker build -t docs -q .) honkit serve
 
-sleep 5
-ls $SCRIPT_DIR
+while [ ! -e "$SCRIPT_DIR/_book/index.html" ]
+do
+    echo "Waiting for server to be up..."
+    sleep 1
+done
+sleep 1
+
 sudo sed -e "s/github.com\/honkit\/honkit/annarbor.hmcc.net/g" -i.backup $SCRIPT_DIR/_book/*.html
 sudo sed -e "s/Published with HonKit/annarbor.hmcc.net/g" -i.backup $SCRIPT_DIR/_book/*.html
 sudo sed -e "s/HonKit<\/title>/HMCC Docs<\/title>/g" -i.backup $SCRIPT_DIR/_book/*.html
