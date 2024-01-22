f3f78a6f714f1cbc3a373fcb9fb24605c6b6fb41
Robert Kang <65458638+rokanq@users.noreply.github.com>
2024-01-22 17:19:29 -0500

    Update gastby-node with page generation code

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -7,12 +7,32 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+ exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  });
+};
+
+//TODO: Modify the following code to pull events from GraphQL before merging
+const path = require("path");
+
+const events = require("./src/data/eventData");
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions;
+
+  events.forEach(event => {
+    createPage({
+      path: `/events/${event.id}`,
+      component: path.resolve(`./src/components/page-events/eventPage.js`),
+      context: {
+        // Passing the entire event object as context
+        event,
+      },
+    });
   });
 };
