be3c1276c5203c8c6945cc3f874e614d78eebde9
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2024-02-05 22:03:35 -0500

    updates for feature tickets

--- frontend/src/components/page-next-steps/transformation.js
+++ frontend/src/components/page-next-steps/transformation.js
@@ -3,8 +3,8 @@ import * as React from "react";
 import { SecondaryButtonLink } from "../Button";
 
 const Transformation = () => (
-  <div className="max-w-container w-full flex flex-col md:flex-row content-image gap-y-5 py-5 text-center md:text-left">
-    <div className="image-container">
+  <div className="max-w-container w-full flex flex-col md:flex-row content-image gap-y-5 py-5 text-center md:text-left items-center">
+    <div className="max-w-[26.25rem] md:max-w-none image-container">
       <StaticImage
         src="../../images/transformation-classes.png"
         alt="Transformation Class"
@@ -15,7 +15,7 @@ const Transformation = () => (
         <div className="subheading">Transformation Classes</div>
         <h2>Dig Deeper.</h2>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col text-base md:text-lg">
         <p>
           Transformation classes dive deeper into some fundamentals of our faith
           - such as what it means to be a disciple of Christ, how to break free
