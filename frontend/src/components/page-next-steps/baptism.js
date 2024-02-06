36dde8cdeaa510090f7eab4876de7b807dd657a9
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2024-02-05 22:03:35 -0500

    updates for feature tickets

--- frontend/src/components/page-next-steps/baptism.js
+++ frontend/src/components/page-next-steps/baptism.js
@@ -3,13 +3,13 @@ import { SecondaryButtonLink } from "../Button";
 
 const Baptism = () => (
   <div className="max-w-container w-full py-5 md:py-15 flex justify-center items-center">
-    <div className="w-full md:border-2 md:border-solid md:border-Neutral-600 py-5 md:py-15 flex text-center px-4 md:px-8 md:rounded-2xl justify-center">
-      <div className="max-w-[50rem] flex flex-col gap-y-5 w-full">
+    <div className="w-full border-2 border-solid border-Neutral-600 pt-[1.625rem] pb-[1.6875rem] md:py-[4.5rem] flex text-center px-6 md:px-8 rounded-2xl justify-center">
+      <div className="max-w-[42.5rem] flex flex-col gap-y-5 w-full">
         <div>
           <div className="subheading">Baptism</div>
           <h2>WHY SHOULD I GET BAPTIZED?</h2>
         </div>
-        <p className="mb-0">
+        <p className="mb-0 text-base md:text-lg">
           Baptism is an outward commitment that you belong with Jesus. It is
           similar to that of a wedding ring, which symbolizes your commitment to
           your partner. If you are a professing Christian, come learn more about
