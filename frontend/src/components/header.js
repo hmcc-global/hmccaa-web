1e8204866e86a0073e60bcf74e409c87fced2a91
Terrian Xiao <88253868+terrianx@users.noreply.github.com>
2024-02-06 14:05:45 -0500

    Make logo style property cleaner
    Co-authored-by: thedomcab <9808478+thedomcab@users.noreply.github.com>

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -28,7 +28,7 @@ const Header = () => {
     (typeof window !== "undefined" && window.location.pathname) || "";
   const containerStyle =
     "flex flex-row h-[60px] lg:h-[100px] relative flext-start bg-Primary-700 items-center justify-between px-4 lg:pt-[40px] lg:pb-[17px] 2xl:px-[65px] [@media(min-width:1140px)]:gap-[40px] [@media(min-width:1440px)]:gap-[100px] [@media(min-width:1600px)]:gap-[210px] position-fixed";
-  const logoStyle = "w-[80px] h-[32px] mb-0";
+  const logoStyle = "w-20 h-8 mb-0";
   const dropDownStyle = "w-[40px] h-[40px] mb-0";
   const textStyle = "text-Shades-0 text-base leading-normal no-underline";
   const mapPinStyle =
