a7a6263c3c9b3b6fb68268f5264e714cd933d771
Justin Lee <justinjl@umich.edu>
2024-02-09 21:10:14 -0500

    83 fix mobile view of home page (#152)
    * Images for get connected
    
    proper functions for mobile and desktop lg
    
    Got a circle, too big on tablet
    
    Fixed mobile margins
    
    Responsive Mobile for Get Connected
    
    Removing old svg files
    
    Using desktop for >1280 resolution
    
    * Removed comments
    
    * Minor fixes for pr
    
    ---------
    
    Co-authored-by: Justin Lee <justin.lee@boncormedical.com>

--- frontend/src/components/page-main/getconnected.js
+++ frontend/src/components/page-main/getconnected.js
@@ -2,26 +2,19 @@ import * as React from "react";
 import { useMediaQuery } from "react-responsive";
 import {
   textContainer,
-  container,
-  ministryTeam,
-  teamWrapper,
-  wrapper,
+  highlightText,
 } from "../../css/ministryTeams.module.css";
 import { SecondaryButtonLink } from "../Button";
-import gc94 from "../../images/gc94.svg";
+import imgAccess from "../../images/home-get-connected/access-raw.png";
+import imgBB from "../../images/home-get-connected/bb-raw.png";
+import imgCovenant from "../../images/home-get-connected/covenant-raw.png";
+import imgFocus from "../../images/home-get-connected/focus-raw.png";
+import imgGA from "../../images/home-get-connected/ga-raw.png";
 import imgImpact from "../../images/home-get-connected/impact-raw.png";
-import ga from "../../images/home-get-connected/ga.svg";
-import lv from "../../images/home-get-connected/lv.svg";
-import covMobile from "../../images/home-get-connected/covenant-mobile.svg";
-import focusMobile from "../../images/home-get-connected/focus-mobile.svg";
-import impactMobile from "../../images/home-get-connected/impact-mobile.svg";
-import accessMobile from "../../images/home-get-connected/access-mobile.svg";
-import gaMobile from "../../images/home-get-connected/ga-mobile.svg";
-import lvMobile from "../../images/home-get-connected/lv-mobile.svg";
-import bbMobile from "../../images/home-get-connected/bb-mobile.svg";
+import imgLatVel from "../../images/home-get-connected/latvel-raw.png";
 
 const GetConnectedText = () => (
-  <div className="w-full flex-col justify-center text-center">
+  <div className="w-full flex-col lg:py-5 justify-center text-center">
     <div className="subheading">Get Connected</div>
     <h2>There is a place for you at HMCC.</h2>
     <p className="pt-21">
@@ -30,54 +23,116 @@ const GetConnectedText = () => (
       different life stages, from youth to students to working adults or married
       couples!
     </p>
-    <div className="flex justify-center">
+    <div className="py-5 flex justify-center">
       <SecondaryButtonLink to="/connect" hasArrow={true}>
         Learn More
       </SecondaryButtonLink>
     </div>
   </div>
 );
-const lgs = [
+
+const lgsArray = [
   {
     title: "COVENANT",
     desc: "MARRIED COUPLES",
     color: "Accent-900", // #a842a8
+    img: imgCovenant,
+    alt: "Covenant LIFE Group",
+    css: `lg:top-[6rem] lg:right-[16rem]`,
   },
   {
     title: "FOCUS",
     desc: "SINGLE ADULTS",
     color: "Accent-700", // #f0508b
+    img: imgFocus,
+    alt: "Focus LIFE Group",
+    css: `bottom-[5rem] right-[18rem]`,
   },
   {
     title: "IMPACT",
     desc: "GRADUATE STUDENTS",
     color: "Accent-300", // #ffbd57
+    img: imgImpact,
+    alt: "Impact LIFE Group",
+    css: `lg:bottom-[18rem] lg:right-[-5rem]`,
   },
   {
     title: "ACCESS",
     desc: "UNDERGRADUATES",
     color: "Error-300", // #f39ea2
+    img: imgAccess,
+    alt: "Access LIFE Group",
+    css: `lg:bottom-[12rem] lg:left-[12rem]`,
   },
   {
     title: "GLOBAL ACCESS",
     desc: "INTERNATIONAL STUDENTS",
     color: "Success-500", // #10b981
+    img: imgGA,
+    alt: "GA LIFE Group",
+    css: `lg:top-[-9rem] lg:left-[20rem]`,
   },
   {
     title: "LATITUDE/VELOCITY",
     desc: "YOUTH",
     color: "Accent-500", // #ff8069
+    img: imgLatVel,
+    alt: "LatVel LIFE Group",
+    css: `lg:top-[12rem] lg:left-[12rem]`,
   },
   {
     title: "BUILDING BLOCKS",
     desc: "KIDS",
-    color: "[#f0df5c]", // #f0df5c
+    color: "[#f0df5c]",
+    img: imgBB,
+    alt: "BB LIFE Group",
+    css: `lg:top-[20rem] lg:right-[-6rem]`,
   },
 ];
+
+const GetConnectedCircleDesktop = () => (
+  <div className="2 w-full pt-[16rem] pb-[8rem] px-10">
+    <div>
+      <div
+        className={
+          "w-full lg:max-w-[56.25rem] lg:relative mx-auto lg:left-[1px] lg:py-[16.6875rem] lg:rounded-full"
+        }
+      >
+        <div className={textContainer}>
+          <GetConnectedText />
+        </div>
+        <div className="flex flex-col absolute top-1/2 left-1/2">
+          {lgsArray.map((item, index) => {
+            const key = `lgs-${index + 1}`;
+            return (
+              <div
+                key={key}
+                className={`w-full lg:w-auto inline-flex justify-center items-center lg:absolute ${item.css}`}
+              >
+                <DoubleGridDesktop
+                  title={item.title}
+                  desc={item.desc}
+                  color={item.color}
+                  img={item.img}
+                  alt={item.alt}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className={highlightText}>
+          Please email <a href="mailto:abc@hmccaa.com">abc@hmccaa.com</a> to get
+          connected!
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 // title on bottom, white text
 // desc on right, white text, left aligned
-const DoubleGridDesktop = ({ title, desc, color }) => (
-  <div className="relative w-[17rem] h-[17.5rem]">
+const DoubleGridDesktop = ({ title, desc, color, img, alt }) => (
+  <div className="w-[17rem] h-[17.5rem]">
     <div
       className={`w-[15.5rem] h-[15.5rem] left-[1.25rem] top-[1.75rem] absolute bg-${color}`}
     >
@@ -91,99 +146,128 @@ const DoubleGridDesktop = ({ title, desc, color }) => (
     <div
       className={`w-[15.5rem] h-[15.5rem] left-0 top-0 absolute border-4 border-${color}`}
     >
-      <img src={imgImpact} alt={"asdf"} className="mb-0 w-full h-full" />
+      <img src={img} alt={alt} className="mb-0 w-full h-full" />
+    </div>
+  </div>
+);
+
+const DoubleGridMobile = ({ title, desc, color, img, alt }) => (
+  <div className="flex-col justify-start items-center gap-1 inline-flex">
+    <div className="text-center w-full text-Shades-100 text-lg font-bold">
+      {title}
+    </div>
+    <div className="relative w-[9.75rem] h-[10rem]">
+      <div
+        className={`w-[9.5rem] h-[9.5rem] left-[.625rem] top-[1rem] absolute bg-${color}`}
+      >
+        <div className="text-center text-sm bottom-[0rem] absolute w-full text-Shades-0 text-md font-bold">
+          {desc}
+        </div>
+      </div>
+      <div
+        className={`w-[9.5rem] h-[9.5rem] left-0 top-0 absolute border-4 border-${color}`}
+      >
+        <img src={img} alt={alt} className="mb-0 w-full h-full" />
+      </div>
+    </div>
+  </div>
+);
+
+const RowMobile = ({ children }) => (
+  <div className="justify-start items-center gap-8 inline-flex">{children}</div>
+);
+
+const LifeStagesBoxMobile = () => (
+  <div className="w-[150px] h-[150px] px-[15px] py-[51px] bg-white justify-center items-center flex">
+    <div className="w-[120px] text-center text-black text-xl font-bold leading-normal">
+      LIFE STAGES
     </div>
   </div>
 );
 
 // title on top, black text
 // desc on bottom, white text, centered
-const DoubleGridMobile = ({ title, desc, color }) => (
-  <div className="flex-col justify-start items-center gap-1 inline-flex">
-    <div className="text-center w-full text-Shades-100 text-lg font-bold">
-      {title}
-    </div>
-    <div className="relative w-[17rem] h-[17.5rem]">
-      <div
-        className={`w-[15.5rem] h-[15.5rem] left-[1.25rem] top-[1.75rem] absolute bg-${color}`}
-      >
-        <div className="text-center bottom-[0rem] absolute w-full text-Shades-0 text-md font-bold">
-          {desc}
-        </div>
-      </div>
-      <div
-        className={`w-[15.5rem] h-[15.5rem] left-0 top-0 absolute border-4 border-${color}`}
-      >
-        <img src={imgImpact} alt={"asdf"} className="mb-0 w-full h-full" />
-      </div>
-    </div>
-  </div>
-);
-
-const GetConnectedCircleDesktop = () => (
-  <div className="flex-col relative">
-    <div className="px-10">
-      <img
-        className="padding-4"
-        alt="Images of LIFE Groups arranged in a circle"
-        src={gc94}
-      />
-    </div>
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+const GetConnectedCircleMobile = () => (
+  <div className="flex-col mx-3 px-4 py-10 w-full inline-flex">
+    <div>
       <GetConnectedText />
     </div>
-  </div>
-);
-
-const GetConnectedCircleMobile = () => (
-  <div className="flex-col justify-start items-center gap-5 inline-flex">
-    <div className="justify-start items-center gap-4 inline-flex">
-      <div className="w-[150px] h-[150px] px-[15px] py-[51px] bg-white justify-center items-center flex">
-        <div className="w-[120px] text-center text-black text-xl font-bold leading-normal">
-          LIFE STAGES
-        </div>
-      </div>
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={covMobile} />
-      </div>
-    </div>
-    <div className="justify-start items-center gap-4 inline-flex">
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={focusMobile} />
-      </div>
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={impactMobile} />
-      </div>
-    </div>
-    <div className="justify-start items-center gap-4 inline-flex">
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={accessMobile} />
-      </div>
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={gaMobile} />
-      </div>
-    </div>
-    <div className="justify-start items-center gap-4 inline-flex">
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={lvMobile} />
-      </div>
-      <div className="flex-col justify-start items-center gap-1 inline-flex">
-        <img src={bbMobile} />
-      </div>
+    <div className="flex-col justify-start items-center gap-4 inline-flex">
+      <RowMobile>
+        <LifeStagesBoxMobile />
+        <DoubleGridMobile
+          title={lgsArray[0].title}
+          desc={lgsArray[0].desc}
+          color={lgsArray[0].color}
+          img={lgsArray[0].img}
+          alt={lgsArray[0].alt}
+        />
+      </RowMobile>
+      <RowMobile>
+        <DoubleGridMobile
+          title={lgsArray[1].title}
+          desc={lgsArray[1].desc}
+          color={lgsArray[1].color}
+          img={lgsArray[1].img}
+          alt={lgsArray[1].alt}
+        />
+        <DoubleGridMobile
+          title={lgsArray[2].title}
+          desc={lgsArray[2].desc}
+          color={lgsArray[2].color}
+          img={lgsArray[2].img}
+          alt={lgsArray[2].alt}
+        />
+      </RowMobile>
+      <RowMobile>
+        <DoubleGridMobile
+          title={lgsArray[3].title}
+          desc={lgsArray[3].desc}
+          color={lgsArray[3].color}
+          img={lgsArray[3].img}
+          alt={lgsArray[3].alt}
+        />
+        <DoubleGridMobile
+          title={lgsArray[4].title}
+          desc={lgsArray[4].desc}
+          color={lgsArray[4].color}
+          img={lgsArray[4].img}
+          alt={lgsArray[4].alt}
+        />
+      </RowMobile>
+      <RowMobile>
+        <DoubleGridMobile
+          title={lgsArray[5].title}
+          desc={lgsArray[5].desc}
+          color={lgsArray[5].color}
+          img={lgsArray[5].img}
+          alt={lgsArray[5].alt}
+        />
+        <DoubleGridMobile
+          title={lgsArray[6].title}
+          desc={lgsArray[6].desc}
+          color={lgsArray[6].color}
+          img={lgsArray[6].img}
+          alt={lgsArray[6].alt}
+        />
+      </RowMobile>
     </div>
   </div>
 );
 
 const GetConnectedCircle = () => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  // Define your switch cases based on screen size
+  // TODO: DoubleGrids aren't responsive
+  // Desktop larger than 1280
+  // but Mobile looks small above 780
+  const isMobile = useMediaQuery({ maxWidth: 1279 });
+  // Define switch cases based on screen size
   const switchElement = isMobile ? (
     <GetConnectedCircleMobile />
   ) : (
     <GetConnectedCircleDesktop />
   );
 
-  return <div>{switchElement}</div>;
+  return switchElement;
 };
 
 export default GetConnectedCircle;
