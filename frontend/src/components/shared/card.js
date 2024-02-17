fd8bd8045dcbe2f06c6c5042b09d62247506123c
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2024-02-17 00:30:06 -0500

    updates for sermons card date and mobile youtube

--- frontend/src/components/shared/card.js
+++ frontend/src/components/shared/card.js
@@ -1,7 +1,15 @@
 import { Link } from "gatsby";
 import * as React from "react";
 
-const Card = ({ img, date, title, children, containerCss, href = "#" }) => {
+const Card = ({
+  img,
+  date,
+  title,
+  children,
+  containerCss,
+  href = "#",
+  graphQLDate,
+}) => {
   function formatDateAndTime(isoDateString) {
     const date = new Date(isoDateString);
 
@@ -39,7 +47,7 @@ const Card = ({ img, date, title, children, containerCss, href = "#" }) => {
         {img}
         <div className="flex flex-col gap-1 items-start md:gap-3 px-2 w-full md:px-[1.8125rem]">
           <p className="mb-0 text-[0.625rem] leading-[0.9375rem] text-black font-medium tracking-[0.0375rem] md:text-base md:leading-[1.3125rem] md:tracking-[0.0525rem]">
-            {formattedDate}
+            {graphQLDate || formattedDate}
           </p>
           <h3 className="text-lg leading-[1.25rem] font-semibold text-black md:leading-[1.8rem] md:text-2xl">
             {title}
