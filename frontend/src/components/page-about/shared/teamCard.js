815af018e03d723f58de76857acadc85660d4278
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2024-02-29 23:13:14 -0500

    minimize changes for edit pages additions

--- frontend/src/components/page-about/shared/teamCard.js
+++ frontend/src/components/page-about/shared/teamCard.js
@@ -45,7 +45,6 @@ const TeamCard = ({ info, customClassName, showModal = false }) => {
     },
     [setMember, setModalOpen]
   );
-
   return (
     <>
       <div className="flex flex-col items-start gap-y-3">
@@ -105,9 +104,7 @@ const TeamCard = ({ info, customClassName, showModal = false }) => {
             </div>
             <div className="flex flex-col lg:flex-row gap-5 pl-[1.125rem] pr-4 pb-[3.375rem] lg:px-[3.125rem] lg:pb-[6.1875rem]">
               <div className="sm:min-w-[280px] flex justify-center">
-                <div>
-                  <img src={member?.img} alt={member?.name} className="mb-0" />
-                </div>
+                <img src={member?.img} alt={member?.name} className="mb-0" />
               </div>
               <div>
                 <div className="flex flex-col gap-y-5">
@@ -118,9 +115,7 @@ const TeamCard = ({ info, customClassName, showModal = false }) => {
                       </h3>
                       <span>({member?.role})</span>
                     </div>
-                    {member?.description.map((text, index) => (
-                      <p key={`paragraph-${index}`}>{text}</p>
-                    ))}
+                    <p>{member?.description}</p>
                     <span>{member?.responsibility}</span>
                   </div>
                   {member?.email && (
