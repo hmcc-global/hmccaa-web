0406e56e5d41646d35ea1016b084ad1825f8f75b
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2024-03-10 17:14:54 -0400

    minimize changes

--- frontend/src/components/page-next-steps/missionsNextSteps.js
+++ frontend/src/components/page-next-steps/missionsNextSteps.js
@@ -1,30 +1,29 @@
 import * as React from "react";
 import { HighlightedParagraph } from "../shared/highlightedParagraph";
 import { StaticImage } from "gatsby-plugin-image";
-import { SecondaryButtonLink } from "../Button";
 
 const MissionsNextStep = () => (
-  <div className="flex flex-col lg:flex-row gap-5 pb-[29px] pt-4 content-image">
-    <div className="lg:order-1 text-center image-container">
+  <div className="flex flex-col lg:flex-row gap-5 pb-[29px] pt-4 lg:py-14">
+    <div className="lg:w-sm lg:order-1 text-center">
       <StaticImage
         src="../../images/missionsNextStepImg.png"
         alt="Missions Next Step"
       />
     </div>
-    <div className="text-center lg:text-left content-container">
+    <div className="lg:w-sm h-auto text-center lg:text-left">
       <div className="subheading">Missions</div>
       <h2>go to all nations</h2>
       <HighlightedParagraph>
         <p className="pt-5">
-          We believe <span>God has called each one of us to missions,</span>
-          whether it’s overseas or locally. As our vision is to be a
-          transcultural, allowing us to share the Gospel wherever we go, to
-          people of all nations. We go in order to experience new cultures and
-          way of life, to see and understand, to empathize and to connect people
-          with Jesus.
+          We believe <span>God has called each one of us to missions</span>,
+          whether it’s overseas or locally. As our vision is to become more
+          transcultural, we want to go where people are. We go in order to
+          experience their culture and way of life, to see what they see and
+          understand what they may encounter each day, to empathize and to
+          connect.
         </p>
         <p>
-          Throughout the years, we’ve traveled to Indonesia, Singapore, Chile,
+          Throughout the years, we’ve travelled to Indonesia, Singapore, Chile,
           Honduras, Peru, Jordan, as well as locally in Detroit, Chicago,
           Austin, and more.
         </p>
@@ -32,10 +31,10 @@ const MissionsNextStep = () => (
           We have new opportunities each year. See how you can get involved!
         </p>
       </HighlightedParagraph>
-      <div className="flex">
-        <SecondaryButtonLink to="" hasArrow={true}>
-          Learn More
-        </SecondaryButtonLink>
+      <div>
+        <p className="text-Accent-200 font-bold">
+          Please email annarbor@hmcc.net to get connected!
+        </p>
       </div>
     </div>
   </div>
