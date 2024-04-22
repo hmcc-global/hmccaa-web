c88b06eb7f567fd1188cf69a60829aed9bf4c3c0
terrianx <txiaopk@gmail.com>
2024-04-22 17:14:01 -0400

    Update home text positioning

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -21,11 +21,11 @@ const IndexPage = () => (
         </h1>
       </div>
       <div className="w-full bg-Neutral-200 justify-center flex px-4">
-        <div className="flex md:flex-row flex-col justify-between items-center py-[50px] max-w-container w-full gap-4">
-          <h2 className="mb-0 xl:text-[60px] lg:text-[55px] md:text-[50px] sm:text-[45px] text-[40px] sm:text-left text-center font-bold leading-tight tracking-[3.6px]">
+        <div className="flex md:flex-row flex-col justify-center items-center py-[50px] max-w-container w-full lg:gap-30 md:gap-10 gap-4">
+          <h2 className="flex-1 mb-0 lg:pr-4 xl:text-[60px] lg:text-[48px] md:text-[44px] sm:text-[40px] text-[35px] md:text-right text-center font-bold leading-tight tracking-[3.6px]">
             JOIN US THIS <br /> SUNDAY!
           </h2>
-          <div className="flex flex-col items-center md:items-start">
+          <div className="flex-1 flex flex-col items-center md:items-start lg:pl-4">
             <div className="font-bold">SERVICE TIMES</div>
             <div className="font-bold text-3xl mb-2">10:00 AM ET</div>
             <div className="flex sm:justify-start justify-center text-center ">
