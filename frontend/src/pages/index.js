29b5a08d0cca357f0a46aed859c956eacca7b583
terrianx <txiaopk@gmail.com>
2024-04-22 22:54:06 -0400

    Change text to use rem instead of px

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -16,13 +16,13 @@ const IndexPage = () => (
           className="w-full h-full mb-0"
           src="../images/image245.png"
         />
-        <h1 className="absolute mb-0 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 line-clamp-2 xl:text-[60px] lg:text-[50px] md:text-[40px] sm:text-[35px] w-full text-[25px] font-bold leading-tight">
+        <h1 className="absolute mb-0 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 line-clamp-2 xl:text-[3.75rem] lg:text-[3.125rem] md:text-[2.5rem] sm:text-[2rem] w-full text-[1.5rem] font-bold leading-tight">
           Transforming lives, <br /> Transforming the world.
         </h1>
       </div>
       <div className="w-full bg-Neutral-200 justify-center flex px-4">
         <div className="flex md:flex-row flex-col justify-center items-center py-[50px] max-w-container w-full lg:gap-30 md:gap-10 gap-4">
-          <h2 className="flex-1 mb-0 lg:pr-4 xl:text-[60px] lg:text-[48px] md:text-[44px] sm:text-[40px] text-[35px] md:text-right text-center font-bold leading-tight tracking-[3.6px]">
+          <h2 className="flex-1 mb-0 lg:pr-4 xl:text-[3.75rem] lg:text-[3rem] md:text-[2.75rem] sm:text-[2.5rem] text-[2rem] md:text-right text-center font-bold leading-tight tracking-[3.6px]">
             JOIN US THIS <br /> SUNDAY!
           </h2>
           <div className="flex-1 flex flex-col items-center md:items-start lg:pl-4">
