931ca8a451eb1063848415bfe63e080fe6f8a925
Maple Xu <mapxu@microsoft.com>
2024-07-12 20:54:24 +0000

    clean up more backend requirements

--- cms/src/api/staff/controllers/staff.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * staff controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::staff.staff');
--- cms/src/api/staff/services/staff.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * staff service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::staff.staff');
