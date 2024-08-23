59aba49367cee60139500c92ae0f9e3fcf79d117
thedomcab <9808478+thedomcab@users.noreply.github.com>
2024-08-23 13:56:49 -0400

    Adding Notification Bar to Home Page (#413)
    * adding banner to home page for lg signup
    
    * updates for responsive web design

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,10 +7,34 @@ import WelcomeMain from "../components/page-main/welcomeMain";
 import UpcomingEvents from "../components/page-main/upcoming";
 import GetConnectedCircle from "../components/page-main/getconnected";
 import GetInvolved from "../components/page-main/getinvolved";
+import Link from "../components/Link";
 
 const IndexPage = () => (
   <Layout>
     <div className="w-full">
+      <div className="bg-Accent-50 text-[#2f3300] font-medium text-xl text-center py-[0.9375rem] flex justify-center">
+        <div className="sm:flex sm:gap-4 lg:items-center lg:max-w-none px-2 [@media(min-width:425px)]:px-8 [@media(min-width:550px)]:px-24 sm:px-16 lg:px-4 text-center sm:text-left lg:text-center">
+          <div className="inline">
+            <StaticImage
+              src="../images/icons/bell.png"
+              alt="bell icon"
+              className="w-[1.875rem]"
+            />
+          </div>
+          <div className="inline lg:flex lg:gap-4">
+            <div className="inline pr-4 lg:px-0 pl-4 sm:pl-0">
+              <span className="font-bold">Life Group</span> is one of the best
+              ways to experience community at HMCC.{" "}
+            </div>
+            <Link
+              to="/next-steps/lifegroups"
+              className="text-Accent-500 underline font-bold whitespace-nowrap inline"
+            >
+              Sign up today!
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className="relative text-center text-Shades-0">
         <StaticImage
           className="w-full h-full mb-0"
