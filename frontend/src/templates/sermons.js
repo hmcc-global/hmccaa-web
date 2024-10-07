c37cf0b017b256c0a06d2d9e806b55c725eb79f4
xumaple <45406854+xumaple@users.noreply.github.com>
2024-10-07 16:28:45 -0400

    add image url fallback (#456)

--- frontend/src/templates/sermons.js
+++ frontend/src/templates/sermons.js
@@ -8,32 +8,6 @@ import Sermons from "../components/page-watch/index/sermons";
 import LiveStream from "../components/page-watch/liveStream";
 
 const WatchPage = ({ data, pageContext }) => {
-  let ids = data.allStrapiSermon?.nodes.map(node => node.strapiId);
-  if (
-    pageContext.skip === 0 &&
-    ids.includes(326) &&
-    ids.includes(321) &&
-    ids.includes(322) &&
-    ids.includes(323) &&
-    ids.includes(324) &&
-    ids.includes(325)
-  ) {
-    console.log(
-      "watch page data:",
-      data.allStrapiSermon?.nodes[0],
-      data.allStrapiSermon?.nodes[0].Series.Background?.file || `0: null`,
-      data.allStrapiSermon?.nodes[1],
-      data.allStrapiSermon?.nodes[1].Series.Background?.file || `1: null`,
-      data.allStrapiSermon?.nodes[2],
-      data.allStrapiSermon?.nodes[2].Series.Background?.file || `2: null`,
-      data.allStrapiSermon?.nodes[3],
-      data.allStrapiSermon?.nodes[3].Series.Background?.file || `3: null`,
-      data.allStrapiSermon?.nodes[4],
-      data.allStrapiSermon?.nodes[4].Series.Background?.file || `4: null`,
-      data.allStrapiSermon?.nodes[5],
-      data.allStrapiSermon?.nodes[5].Series.Background?.file || `5: null`
-    );
-  }
   return (
     <Layout hasSpacing={false}>
       <Banner bgImage="bg-[center_60%] bg-watch">Watch Online</Banner>
@@ -73,6 +47,7 @@ export const pageQuery = graphql`
           Name
           id
           Background {
+            url
             file {
               childImageSharp {
                 gatsbyImageData
