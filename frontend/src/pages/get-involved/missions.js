92ac9a2e05adaf4c990bdf198c5bc22717543906
Maple Xu <maplexu2010@gmail.com>
2024-12-10 18:04:56 +0000

    missions frontend

--- frontend/src/pages/get-involved/missions.js
+++ frontend/src/pages/get-involved/missions.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../../components/layout";
 import { Breadcrumb } from "gatsby-plugin-breadcrumb";
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image";
 import Seo, { PageDescriptions } from "../../components/seo";
 import { CalendarIcon, PinIcon } from "../../components/svgs";
 import { PrimaryButtonLink } from "../../components/Button";
+import { graphql } from "gatsby";
+import { formatEventTimeAsString } from "../../components/page-events/event-processing-util";
+import RichText from "../../components/shared/richText";
+import Link from "../../components/Link";
 
 // show this instead when no current missions
 const MissionsEmpty = () => {
@@ -28,59 +32,50 @@ const MissionsEmpty = () => {
   );
 };
 
+const showImage = image => {
+  let { file, alternativeText } = image;
+  return file?.childImageSharp?.gatsbyImageData ? (
+    <GatsbyImage
+      image={file?.childImageSharp?.gatsbyImageData}
+      alt={"hey"}
+      className="h-full"
+    />
+  ) : (
+    <div className="h-full w-full"></div>
+  );
+};
+
+const showLocation = ({ LocationName, GoogleMapsLink }) => {
+  return GoogleMapsLink ? (
+    <Link href={GoogleMapsLink}>{LocationName}</Link>
+  ) : (
+    LocationName
+  );
+};
+
 const MissionsPage = ({
+  data,
   pageContext: {
     breadcrumb: { crumbs },
   },
 }) => {
-  const hash = str =>
-    str
-      .split("")
-      .reduce((prev, curr) => ((prev << 5) - prev + curr.charCodeAt(0)) | 0, 0);
-
-  const missions = [
-    // {
-    //   id: 1,
-    //   trip: "Peru Missions",
-    //   dates: "June 7 - June 17, 2023",
-    //   city: "Peru",
-    //   image: (
-    //     <StaticImage
-    //       alt="Peru Team"
-    //       src="../../images/peru-team.png"
-    //       className="w-full"
-    //     />
-    //   ),
-    //   details: [
-    //     "Lorem Ipsum... esus talks about living an abundant life. The vision of LIFE Group is: to experience the fullness of life in a Biblical community where the Gospel is radically lived out. LIFE Groups seek to accomplish this by sharing our lives with one another within various life stages.",
-    //     "We desire to live out the 5 E’s (Evangelism, Education, Edification, Extension, Exaltation) of our values in a community context by studying the Bible, worshiping, serving and praying together. ",
-    //   ],
-    //   images: ["", "", "", "", ""],
-    // },
-    // {
-    //   id: 2,
-    //   trip: "Chicago Missions",
-    //   dates: "June 7 - June 17, 2023",
-    //   city: "Chicago",
-    //   image: (
-    //     <StaticImage
-    //       alt="Chicago Team"
-    //       className="w-full"
-    //       src="../../images/chicago-team-placement.png"
-    //     />
-    //   ),
-    //   details: [
-    //     "Lorem Ipsum... esus talks about living an abundant life. The vision of LIFE Group is: to experience the fullness of life in a Biblical community where the Gospel is radically lived out. LIFE Groups seek to accomplish this by sharing our lives with one another within various life stages.",
-    //     "We desire to live out the 5 E’s (Evangelism, Education, Edification, Extension, Exaltation) of our values in a community context by studying the Bible, worshiping, serving and praying together. ",
-    //   ],
-    //   images: ["", "", "", "", ""],
-    // },
-  ];
+  const missions = data.allStrapiMissionsTrip.nodes.map(obj => ({
+    strapiId: obj.strapiId,
+    name: obj.Name,
+    dates: formatEventTimeAsString(
+      { start: obj.StartDate, end: obj.EndDate },
+      false
+    ),
+    location: showLocation(obj.Location),
+    images: obj.Images,
+    description: obj.Description,
+  }));
+  console.log("data", missions);
 
   return (
     <Layout>
       <div className="pt-[1.375rem] lg:pt-10 pb-[4.8125rem] lg:pb-[2.25rem] content-padding-full gap-y-5 lg:gap-y-15 min-h-screen">
-        <Breadcrumb crumbs={crumbs} crumbSeparator=" > " />
+        {/* <Breadcrumb crumbs={crumbs} crumbSeparator=" > " /> */}
         {missions.length == 0 ? (
           <MissionsEmpty />
         ) : (
@@ -90,18 +85,21 @@ const MissionsPage = ({
             </h1>
             <div className="flex flex-col w-full lg:gap-y-15">
               {missions.map(
-                ({ id, trip, dates, city, image, details, images }, index) => (
+                (
+                  { strapiId, name, dates, location, description, images },
+                  index
+                ) => (
                   <div
                     className={`flex flex-col w-full gap-y-5 lg:gap-y-10 items-center lg:items-start pt-4 lg:pt-0 ${
                       index + 1 < missions.length
                         ? "pb-6 lg:pb-[4.875rem] border-b-solid border-b-Shades-100 border-b-4"
                         : ""
                     }`}
-                    key={id}
+                    key={strapiId}
                   >
                     <div className="w-full gap-y-5 gap-x-15 flex flex-col lg:flex-row items-center lg:items-start">
                       <div className="content-container flex flex-col gap-y-1 lg:gap-y-5">
-                        <h2>{trip}</h2>
+                        <h2>{name}</h2>
                         <div className="flex flex-col lg:gap-y-5 text-base font-medium lg:font-normal lg:text-xl leading-normal items-center lg:items-start">
                           <div className="flex gap-x-1 lg:gap-x-2">
                             <CalendarIcon className="w-5 h-5 lg:w-8 lg:h-8" />
@@ -109,7 +107,7 @@ const MissionsPage = ({
                           </div>
                           <div className="flex gap-x-1 lg:gap-x-2">
                             <PinIcon className="w-5 h-5 lg:w-8 lg:h-8" />
-                            <span>{city}</span>
+                            <span>{location}</span>
                           </div>
                         </div>
                         <div className="flex pt-4 lg:pt-10">
@@ -118,32 +116,25 @@ const MissionsPage = ({
                           </PrimaryButtonLink>
                         </div>
                       </div>
-                      <div className="image-container">{image}</div>
+                      <div className="image-container">
+                        {showImage(images[0])}
+                      </div>
                     </div>
-                    <section className="flex flex-col">
+                    <div className="flex flex-col">
                       <article className="text-[#8d8d8d] text-lg">
-                        {details.map((p, index) => (
-                          <p
-                            key={index}
-                            className={`${
-                              index + 1 === details.length ? "mb-0" : ""
-                            }`}
-                          >
-                            {p}
-                          </p>
-                        ))}
+                        <RichText data={description} />
                       </article>
-                    </section>
-                    <section className="grid grid-cols-2 gap-y-4 gap-x-4 lg:flex  lg:gap-x-5 max-w-[20.5rem] lg:max-w-none w-full">
-                      {images.map(item => (
+                    </div>
+                    <div className="grid grid-cols-2 gap-y-4 gap-x-4 lg:flex  lg:gap-x-5 max-w-[20.5rem] lg:max-w-none w-full">
+                      {images.slice(1).map(image => (
                         <div
-                          className="bg-[#5e5e5e] relative pb-[67.31%] lg:pb-[12.245%] w-full"
-                          key={Math.abs(hash(item))}
+                          className="relative pb-[67.31%] lg:pb-[12.245%] w-full"
+                          key={image.strapiId}
                         >
-                          <div className="absolute left-0 top-0">{item}</div>
+                          <div className="left-0 top-0">{showImage(image)}</div>
                         </div>
                       ))}
-                    </section>
+                    </div>
                   </div>
                 )
               )}
@@ -161,5 +152,37 @@ export const Head = () => (
     description={PageDescriptions["get-involved-missions"]}
   />
 );
+
+export const pageQuery = graphql`
+  query MissionsTripsQuery {
+    allStrapiMissionsTrip {
+      nodes {
+        strapiId
+        Name
+        Description
+        EndDate
+        StartDate
+        Location {
+          GoogleMapsLink
+          LocationName
+        }
+        Images {
+          alternativeText
+          file {
+            childImageSharp {
+              gatsbyImageData
+            }
+            url
+          }
+          strapiId
+        }
+        Links {
+          Hyperlink
+          Text
+        }
+      }
+    }
+  }
+`;
 
 export default MissionsPage;
