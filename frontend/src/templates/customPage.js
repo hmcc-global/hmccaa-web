22967113b3e7c4ff9c7f22f13601089c803f6bac
Dominique Caballero <9808478+thedomcab@users.noreply.github.com>
2025-01-11 18:14:59 -0500

    updates for custom page

--- frontend/src/templates/customPage.js
+++ frontend/src/templates/customPage.js
@@ -12,11 +12,11 @@ const CustomPage = ({ data }) => {
   const bgImageUrl = url ? `${process.env.HOST_ORIGIN}${url}` : null;
   console.log("[CustomPage] Generated banner url:", bgImageUrl || bgImage);
   return (
-    <Layout hasSpacing={false}>
+    <Layout hasSpacing={false} className="custom-page">
       <Banner bgImage={`bg-[center_60%] ${bgImage}`} bgImageUrl={bgImageUrl}>
         {pageData?.Title}
       </Banner>
-      <div className="content-padding-full w-3 py-[0.9375rem] px-2 [@media(min-width:425px)]:px-8 [@media(min-width:550px)]:px-24 sm:px-16 lg:px-4 flex gap-y-3 [&>*]:w-full">
+      <div className="content-padding-full w-3 py-[0.9375rem] px-2 [@media(min-width:425px)]:px-8 [@media(min-width:550px)]:px-24 sm:px-16 lg:px-4 flex [&>*]:w-full">
         <RichText data={pageData?.Content} />
       </div>
     </Layout>
