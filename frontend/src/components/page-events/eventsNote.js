04307c556ae4da310d6177170c7020b6fcfb4b86
bbowling <brandt.bowling@gmail.com>
2025-01-22 20:53:50 -0500

    align note

--- frontend/src/components/page-events/eventsNote.js
+++ frontend/src/components/page-events/eventsNote.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const EventsNotes = () => {
   return (
-    <p className="text-left pb-10 lg:pb-20 md:text-xl px-4">
+    <p className="text-left pb-10 lg:pb-20 md:text-lg">
       *Note: <span className="font-bold">Experiencing Ministry</span>,{" "}
       <span className="font-bold">Experiencing Membership</span>, and{" "}
       <span className="font-bold">Baptism classes</span> happens around February
--- frontend/src/pages/events/index.js
+++ frontend/src/pages/events/index.js
@@ -27,51 +27,56 @@ const EventsPage = ({ data }) => {
     <Layout hasSpacing={false}>
       <Banner bgImage="bg-center bg-events">Upcoming Events</Banner>
       <SundayCelebBarEvents />
-      <div className="px-4 w-full flex flex-col items-center">
-        {events.length === 0 ? (
-          <div className="text-center py-36">No events found.</div>
-        ) : (
-          <div className="grid grid-cols-2 md:grid-cols-3 md:auto-rows-[30rem] gap-x-5 gap-y-[2.0625rem] md:gap-y-15 pt-8 pb-9 md:py-10 max-w-container">
-            {events
-              .map(event => event.times.map(time => ({ event, time })))
-              .flat()
-              .sort((a, b) => sortTimes(a.time, b.time))
-              .map(({ event, time }) => {
-                let key = encodeURIComponent(getFullEventId(event.id, time));
-                return (
-                  <EventCard
-                    key={key}
-                    eventID={key}
-                    title={event.title}
-                    time={formatEventTimeAsString(time)}
-                    img={
-                      <div className="relative pb-[63.493%] w-full">
-                        <div className="absolute inset-0">
-                          {event.img?.file?.childImageSharp?.gatsbyImageData ? (
-                            <GatsbyImage
-                              image={
-                                event.img?.file?.childImageSharp
-                                  ?.gatsbyImageData
-                              }
-                              alt={event.imgAlt}
-                              className="h-full"
-                            />
-                          ) : (
-                            <div className="h-full w-full"></div>
-                          )}
+      <div className="px-4 w-full flex flex-col ">
+        <div className="flex flex-col items-center">
+          {events.length === 0 ? (
+            <div className="text-center py-36">No events found.</div>
+          ) : (
+            <div className="grid grid-cols-2 md:grid-cols-3 md:auto-rows-[30rem] gap-x-5 gap-y-[2.0625rem] md:gap-y-15 pt-8 pb-9 md:py-10 max-w-container">
+              {events
+                .map(event => event.times.map(time => ({ event, time })))
+                .flat()
+                .sort((a, b) => sortTimes(a.time, b.time))
+                .map(({ event, time }) => {
+                  let key = encodeURIComponent(getFullEventId(event.id, time));
+                  return (
+                    <EventCard
+                      key={key}
+                      eventID={key}
+                      title={event.title}
+                      time={formatEventTimeAsString(time)}
+                      img={
+                        <div className="relative pb-[63.493%] w-full">
+                          <div className="absolute inset-0">
+                            {event.img?.file?.childImageSharp
+                              ?.gatsbyImageData ? (
+                              <GatsbyImage
+                                image={
+                                  event.img?.file?.childImageSharp
+                                    ?.gatsbyImageData
+                                }
+                                alt={event.imgAlt}
+                                className="h-full"
+                              />
+                            ) : (
+                              <div className="h-full w-full"></div>
+                            )}
+                          </div>
                         </div>
-                      </div>
-                    }
-                    location={event.location}
-                    description={event.description[0]}
-                  />
-                );
-              })}
-          </div>
-        )}
+                      }
+                      location={event.location}
+                      description={event.description[0]}
+                    />
+                  );
+                })}
+            </div>
+          )}
+        </div>
+        <div className="xl:ml-[calc((100vw_-_75.75rem)/2)]">
+          <EventsNotes />
+        </div>
       </div>
-      <EventsNotes />
-      <PrayerGatheringEvents />
+      <divPrayerGatheringEvents />
       <Instagram />
     </Layout>
   );
