c812972165ddbb30e3d11ab6ea97450bc4e5c849
xumaple <45406854+xumaple@users.noreply.github.com>
2025-03-01 22:07:55 -0500

    Add cms section for supported missionaries (#519)

---
+++ cms/src/api/supported-missionary/controllers/supported-missionary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * supported-missionary controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::supported-missionary.supported-missionary');
---
+++ cms/src/api/supported-missionary/routes/supported-missionary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * supported-missionary router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::supported-missionary.supported-missionary');
