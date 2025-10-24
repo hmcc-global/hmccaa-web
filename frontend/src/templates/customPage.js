1640f183660c2fcfe8596517176f2ebf2fb05347
Maple Xu <maplexu2010@gmail.com>
2025-10-24 15:24:10 -0400

    center align images and add in button

--- frontend/src/templates/customPage.js
+++ frontend/src/templates/customPage.js
@@ -4,12 +4,15 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Banner from "../components/shared/banner";
 import RichText from "../components/shared/richText";
+import { PrimaryButtonLink } from "../components/Button";
 
 const CustomPage = ({ data }) => {
   const pageData = data.strapiCustomPage;
   const url = pageData?.BannerImage?.file?.publicURL;
   const bgImage = url ? "" : "bg-events";
   const bgImageUrl = url ? `${process.env.HOST_ORIGIN}${url}` : null;
+  const buttonText = pageData?.BottomButton?.Text;
+  const buttonLink = pageData?.BottomButton?.Hyperlink;
   console.log("[CustomPage] Generated banner url:", bgImageUrl || bgImage);
   return (
     <Layout hasSpacing={false} className="custom-page">
@@ -19,6 +22,13 @@ const CustomPage = ({ data }) => {
       <div className="content-padding-full w-3 py-[1.5375rem] px-2 [@media(min-width:425px)]:px-8 [@media(min-width:550px)]:px-24 sm:px-16 lg:px-4 flex [&>*]:w-full custom-content">
         <RichText data={pageData?.Content} />
       </div>
+      {buttonText && buttonLink && (
+        <div className="sm:flex sm:justify-center xl:relative z-10 pb-8">
+          <PrimaryButtonLink hasArrow={true} href={buttonLink}>
+            {buttonText}
+          </PrimaryButtonLink>
+        </div>
+      )}
     </Layout>
   );
 };
