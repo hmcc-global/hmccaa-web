d6e63514963e9349c075676fa1301c01a9adcca0
Maple Xu <maplexu2010@gmail.com>
2025-10-26 03:54:10 -0400

    track memory

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -25,9 +25,56 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   await CreateEventPages(graphql, createPageAndPrintFn, reporter);
 
-  // await CreateSermonPages(graphql, createPageFn, reporter);
+  await CreateSermonPages(graphql, createPageFn, reporter);
 
   await CreateCustomPages(graphql, createPageAndPrintFn, reporter);
 
   pages.createPages();
+};
+
+// gatsby-node.js
+
+function logMemory(label) {
+  const used = process.memoryUsage();
+  console.log(
+    `[${label}] Memory usage: rss=${(used.rss / 1024 / 1024).toFixed(
+      2
+    )} MB, heapUsed=${(used.heapUsed / 1024 / 1024).toFixed(2)} MB`
+  );
+}
+
+exports.onPreInit = () => {
+  logMemory("onPreInit");
+};
+
+exports.onPreBootstrap = () => {
+  logMemory("onPreBootstrap");
+};
+
+exports.sourceNodes = async (args, options) => {
+  logMemory("before sourceNodes");
+  // Let other plugins run their sourceNodes
+};
+
+exports.onCreateNode = ({ node, actions }) => {
+  if (node.internal && node.internal.type) {
+    // Occasionally log node types to see if one type is exploding in count
+    if (Math.random() < 0.0005) {
+      console.log(`Node created: ${node.internal.type}`);
+    }
+  }
+};
+
+exports.onPostBootstrap = ({ getNodes }) => {
+  logMemory("onPostBootstrap");
+  console.log("Total nodes so far:", getNodes().length);
+};
+
+exports.onPreExtractQueries = () => {
+  logMemory("onPreExtractQueries");
+};
+
+exports.onPostBuild = ({ getNodes }) => {
+  logMemory("onPostBuild");
+  console.log("Final node count:", getNodes().length);
 };
