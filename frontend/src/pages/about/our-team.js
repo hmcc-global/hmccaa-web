d1f611ab387e0d9c2fbd44b77b4bdc7dd426038a
Maple Xu <maplexu2010@gmail.com>
2026-02-26 20:31:36 +0000

    Our Team page uses CMS

--- frontend/src/pages/about/our-team.js
+++ frontend/src/pages/about/our-team.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Breadcrumb } from "gatsby-plugin-breadcrumb";
+import { graphql } from "gatsby";
 
 import Layout from "../../components/layout";
 import Seo, { PageDescriptions } from "../../components/seo";
@@ -8,18 +9,21 @@ import EldersSection from "../../components/page-about/team/eldersSection";
 import DeaconsSection from "../../components/page-about/team/deaconsSection";
 import Questions from "../../components/gotQuestions";
 
-const OurTeamPage = ({ pageContext }) => {
-  const {
+const OurTeamPage = ({
+  data: { strapiOurTeamPage },
+  pageContext: {
     breadcrumb: { crumbs },
-  } = pageContext;
+  },
+}) => {
+  const { Elders, deacons } = strapiOurTeamPage;
   return (
     <Layout>
       <div className="px-4 flex flex-col w-full items-center pt-[1.375rem] lg:pt-10 gap-y-[2.1875rem] lg:gap-y-[3.75rem]">
         <Breadcrumb crumbs={crumbs} crumbSeparator=" > " />
         <div className="max-w-container w-full flex flex-col items-center gap-y-[3.75rem] lg:gap-y-[6.25rem]">
           <Heading />
-          <EldersSection />
-          <DeaconsSection />
+          <EldersSection elders={Elders} />
+          <DeaconsSection deacons={deacons} />
         </div>
         <Questions />
       </div>
@@ -32,3 +36,42 @@ export const Head = () => (
 );
 
 export default OurTeamPage;
+
+export const pageQuery = graphql`
+  query OurTeamQuery {
+    strapiOurTeamPage {
+      Elders {
+        Background
+        Role
+        TeamMember {
+          DisplayName
+          Email
+          Headshot {
+            file {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+          focus_areas {
+            FocusArea
+          }
+        }
+      }
+      deacons {
+        DisplayName
+        Email
+        focus_areas {
+          FocusArea
+        }
+        Headshot {
+          file {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`;
