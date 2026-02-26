9684f3ea117197a12f1a63fc8e02c69f07676525
Maple Xu <maplexu2010@gmail.com>
2026-02-26 17:22:34 +0000

    add cms for deacons page

---
+++ cms/src/api/our-team-page/services/our-team-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-team-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::our-team-page.our-team-page');
